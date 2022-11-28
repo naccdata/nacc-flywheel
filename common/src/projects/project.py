0626c7283a4cf218b60ccba2b788e602780cd397
Ben Keller <bjkeller@vt.edu>
2022-11-28 21:56:05 +0000

    Update project classes
    Update project classes
    1. Add block comments
    2. apply formatter
    3. Use simple slug creation

--- common/src/projects/project.py
+++ common/src/projects/project.py
@@ -1,20 +1,32 @@
-"""
-Classes for representing NACC projects.
-"""
+"""Classes for representing NACC projects."""
+import re
 from abc import ABC, abstractmethod
 from typing import List, Mapping, TypeVar
 
-from slugify import slugify
+
+def convert_to_slug(name: str) -> str:
+    """Converts center name to a slug for use as a human-readable ID.
+
+    Removes non-word, non-whitespace characters, replaces runs of
+    whitespace with a single hyphen, and returns in lower case.
+
+    Args:
+      name: the name of the center
+
+    Returns:
+      The transformed name.
+    """
+    name = re.sub(r"[^\w\s]", '', name)
+    name = re.sub(r"\s+", '-', name)
+    return name.lower()
 
 
 class ProjectVisitor(ABC):
-    """
-    Abstract class for a visitor object for projects.
-    """
+    """Abstract class for a visitor object for projects."""
+
     @abstractmethod
     def visit_project(self, project: "Project") -> None:
-        """
-        Method to visit the given project.
+        """Method to visit the given project.
 
         Args:
           project: the project to visit.
@@ -22,8 +34,7 @@ class ProjectVisitor(ABC):
 
     @abstractmethod
     def visit_center(self, center: "Center") -> None:
-        """
-        Method to visit the given center within a project.
+        """Method to visit the given center within a project.
 
         Args:
           center: the center to visit
@@ -31,8 +42,7 @@ class ProjectVisitor(ABC):
 
     @abstractmethod
     def visit_datatype(self, datatype: str):
-        """
-        Method to visit the given datatype within a project.
+        """Method to visit the given datatype within a project.
 
         Args:
           datatype: the name of the datatype within a project.
@@ -40,7 +50,7 @@ class ProjectVisitor(ABC):
 
 
 class Center:
-    """Represents a center with data managed at NACC"""
+    """Represents a center with data managed at NACC."""
 
     def __init__(self, *, adcid: int, name: str, active: bool = True) -> None:
         self._adcid = adcid
@@ -48,39 +58,56 @@ class Center:
         self._active = active
 
     def __repr__(self) -> str:
-        return f"Center(adcid={self.adcid}, center_id={self.center_id}, name={self.name})"
+        return (f"Center(adcid={self.adcid}, "
+                f"center_id={self.center_id}, "
+                f"name={self.name})")
 
     def __eq__(self, __o: object) -> bool:
         if not isinstance(__o, Center):
             return False
-        return (self._adcid == __o._adcid and
-                self._active == __o._active and
-                self.name == __o.name)
+        return (self._adcid == __o._adcid and self._active == __o._active
+                and self.name == __o.name)
 
     @property
     def name(self):
+        """Center name property."""
         return self._name
+
+    def is_active(self):
+        """Indicates whether the center is active."""
+        return self._active
 
     @property
     def adcid(self):
+        """Center ADC ID property."""
         return self._adcid
 
     @property
     def center_id(self):
-        return slugify(self._name)
+        """Center text ID property."""
+        return convert_to_slug(self._name)
 
     def apply(self, visitor):
+        """Applies visitor to this Center."""
         visitor.visit_center(self)
 
     @classmethod
     def create(cls, center: dict) -> "Center":
-        return Center(adcid=center['adc-id'], name=center['name'], active=center['is-active'])
+        """Creates a Center from the given dictionary."""
+        return Center(adcid=center['adc-id'],
+                      name=center['name'],
+                      active=center['is-active'])
 
 
 class Project:
-    """Represents a Project with data managed at NACC"""
+    """Represents a Project with data managed at NACC."""
 
-    def __init__(self, *, name: str, centers: List[Center], datatypes: List[str], published: bool = False) -> None:
+    def __init__(self,
+                 *,
+                 name: str,
+                 centers: List[Center],
+                 datatypes: List[str],
+                 published: bool = False) -> None:
         self._name = name
         self._centers = centers
         self._datatypes = datatypes
@@ -88,30 +115,38 @@ class Project:
 
     @property
     def project_id(self) -> str:
+        """Project ID property."""
         return self._name
 
     @property
     def name(self) -> str:
+        """Project Name property."""
         return self._name
 
     @property
     def centers(self) -> List[Center]:
+        """Project centers property."""
         return self._centers
 
     @property
     def datatypes(self) -> List[str]:
+        """Project datatypes property."""
         return self._datatypes
 
-    @property
-    def published(self) -> bool:
-        return self.published
+    def is_published(self) -> bool:
+        """Project published predicate."""
+        return self._published
 
     def apply(self, visitor) -> None:
+        """Apply visitor to this Project."""
         visitor.visit_project(self)
 
     T = TypeVar('T')
 
     @classmethod
     def create(cls, project: Mapping[str, T]) -> "Project":
-        # is this actually a list ?
-        return Project(name=project['project'], centers=project['centers'], datatypes=project['datatypes'], published=project['published'])
+        """Create Project from given mapping."""
+        return Project(name=project['project'],
+                       centers=project['centers'],
+                       datatypes=project['datatypes'],
+                       published=project['published'])
