d69943d0e03bf6f788d3833e75c5915c812c218a
Ben Keller <bjkeller@vt.edu>
2022-11-29 04:53:54 +0000

    Add tests for project class

--- common/test/test_project.py
+++ common/test/test_project.py
@@ -1,4 +1,5 @@
 """Tests for projects.*"""
+import pytest
 from projects.project import Center, Project, ProjectVisitor
 
 
@@ -7,15 +8,17 @@ class DummyVisitor(ProjectVisitor):
 
     def __init__(self) -> None:
         self.center_name = None
+        self.project_name = None
+        self.datatype_name = None
 
     def visit_center(self, center: Center) -> None:
         self.center_name = center.name
 
     def visit_datatype(self, datatype: str):
-        return
+        self.datatype_name = datatype
 
     def visit_project(self, project: Project) -> None:
-        return
+        self.project_name = project.name
 
 
 # pylint: disable=(no-self-use)
@@ -40,9 +43,57 @@ class TestCenter:
         center2 = Center(adcid=7, name="Alpha ADRC")
         assert center == center2
 
+        with pytest.raises(KeyError):
+            Center.create({})
+
     def test_apply(self):
         """Test that visitor applied."""
         visitor = DummyVisitor()
         center = Center(adcid=1, name="Dummy Center")
         center.apply(visitor)
         assert visitor.center_name == "Dummy Center"
+
+
+class TestProject:
+    """Tests for Project class."""
+
+    def test_object(self):
+        """Tests for object creation."""
+        project = Project(
+            name="Project Alpha",
+            centers=[Center(adcid=1, name='A Center', active=True)],
+            datatypes=['dicom'],
+            published=True)
+        assert project.project_id == "project-alpha"
+        assert project.centers == [
+            Center(adcid=1, name='A Center', active=True)
+        ]
+        assert project.datatypes == ['dicom']
+        assert project.is_published()
+
+        project2 = Project.create({
+            'project':
+            'Project Alpha',
+            'centers': [{
+                'adc-id': 1,
+                'name': 'A Center',
+                'is-active': True
+            }],
+            'datatypes': ['dicom'],
+            'published':
+            True
+        })
+        assert project == project2
+
+        with pytest.raises(KeyError):
+            Project.create({})
+
+    def test_apply(self):
+        """Test project apply method."""
+        visitor = DummyVisitor()
+        project = Project(name='Project Beta',
+                          centers=[],
+                          datatypes=[],
+                          published=True)
+        project.apply(visitor)
+        assert visitor.project_name == 'Project Beta'
