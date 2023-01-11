f1b4bd14a2975206e29b59691ee976e6038be125
Ben Keller <bjkeller@vt.edu>
2023-01-11 15:26:58 +0000

    Change name of usermanagement main

--- user_management/src/python/user_main.py
+++ user_management/src/python/user_main.py
@@ -1,16 +1,20 @@
-
 import logging
+from typing import List
+
+import flywheel
+from projects.flywheel_proxy import FlywheelProxy
 
 log = logging.getLogger(__name__)
 
-def run(*, api_key: str, user_list, dry_run: bool = False):
+
+def run(*, proxy: FlywheelProxy, user_list, admin_users: List[flywheel.User]):
     """does the work."""
 
-    # flywheel_proxy = FlywheelProxy(api_key=api_key, dry_run=dry_run)
+    for user in admin_users:
+        log.info("User: %s", user)
+
     # visitor = FlywheelProjectArtifactCreator(flywheel_proxy)
     for user_doc in user_list:
         # project = Project.create(user_doc)
         # project.apply(visitor)
         pass
-
-
