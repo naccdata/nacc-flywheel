2b50cab6229a04345e5b03c78607942d0f12ffa8
Ben Keller <bjkeller@vt.edu>
2023-01-11 21:55:02 +0000

    Refactor user management scripts

--- user_management/src/python/run.py
+++ user_management/src/python/run.py
@@ -3,17 +3,21 @@
 import logging
 import sys
 
-from inputs.context_parser import parse_config
-from inputs.environment import get_api_key
+from admin.users import get_admin_users
 from flywheel_gear_toolkit import GearToolkitContext
 from inputs.arguments import build_parser
+from inputs.context_parser import parse_config
+from inputs.environment import get_api_key
 from inputs.yaml import get_object_list
-from main import run
+from projects.flywheel_proxy import FlywheelProxy
+from user_main import run
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger(__name__)
 
+
 def main() -> None:
-    """Check arguments."""
+    """Main method to manage users."""
 
     parser = build_parser()
     args = parser.parse_args()
@@ -22,12 +26,15 @@ def main() -> None:
         filename = 'user_file'
         with GearToolkitContext() as gear_context:
             gear_context.init_logging()
-            context_args = parse_config(gear_context=gear_context, filename=filename)
+            context_args = parse_config(gear_context=gear_context,
+                                        filename=filename)
+            admin_group_name = context_args['admin_group']
             dry_run = context_args['dry_run']
             user_file = context_args[filename]
     else:
         dry_run = args.dry_run
         user_file = args.filename
+        admin_group_name = args.admin_group
 
     user_list = get_object_list(user_file)
     if not user_list:
@@ -38,7 +45,12 @@ def main() -> None:
         log.error('No API key: expecting FW_API_KEY to be set')
         sys.exit(1)
 
-    run(api_key=api_key, user_list=user_list, dry_run=dry_run)
+    flywheel_proxy = FlywheelProxy(api_key=api_key, dry_run=dry_run)
+
+    admin_users = get_admin_users(flywheel_proxy=flywheel_proxy,
+                                  group_name=admin_group_name)
+
+    run(proxy=flywheel_proxy, user_list=user_list, admin_users=admin_users)
 
 
 if __name__ == "__main__":
