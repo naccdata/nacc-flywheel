280eed80790cdca8fb7e2fae9e4edef81b639cc8
Ben Keller <bjkeller@vt.edu>
2023-01-11 21:54:28 +0000

    Refactor project management scripts to handle access

--- project_management/src/python/project_main.py
+++ project_management/src/python/project_main.py
@@ -1,17 +1,29 @@
 """Defines project management computation."""
 
+import logging
+from typing import List
+
+import flywheel
 from projects.flywheel_proxy import FlywheelProxy
 from projects.project import Project
 from projects.project_mapping import ProjectMappingAdaptor
 
+log = logging.getLogger(__name__)
 
-def run(*, api_key: str, project_list, dry_run: bool = False):
-    """Parses file and does the work."""
 
-    flywheel_proxy = FlywheelProxy(api_key=api_key, dry_run=dry_run)
+def run(*, proxy: FlywheelProxy, project_list,
+        admin_users: List[flywheel.User]):
+    """Runs project pipeline creation/management.
+
+    Args:
+      proxy: the proxy for the Flywheel instance
+      project_list: the list of project input
+      admin_users: the list of admin users
+    """
 
     for project_doc in project_list:
         project = Project.create(project_doc)
         project_mapper = ProjectMappingAdaptor(project=project,
-                                               flywheel_proxy=flywheel_proxy)
+                                               flywheel_proxy=proxy,
+                                               admin_users=admin_users)
         project_mapper.create_project_pipelines()
--- project_management/src/python/run.py
+++ project_management/src/python/run.py
@@ -11,13 +11,16 @@ published - boolean indicating whether data is to be published
 import logging
 import sys
 
+from admin.users import get_admin_users
 from flywheel_gear_toolkit import GearToolkitContext
 from inputs.arguments import build_parser
 from inputs.context_parser import parse_config
 from inputs.environment import get_api_key
 from inputs.yaml import get_object_list
 from project_main import run
+from projects.flywheel_proxy import FlywheelProxy
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
@@ -33,11 +36,13 @@ def main():
             gear_context.init_logging()
             context_args = parse_config(gear_context=gear_context,
                                         filename=filename)
+            admin_group_name = context_args['admin_group']
             dry_run = context_args['dry_run']
             project_file = context_args[filename]
     else:
         dry_run = args.dry_run
         project_file = args.filename
+        admin_group_name = args.admin_group
 
     project_list = get_object_list(project_file)
     if not project_list:
@@ -48,7 +53,14 @@ def main():
         log.error('No API key: expecting FW_API_KEY to be set')
         sys.exit(1)
 
-    run(api_key=api_key, project_list=project_list, dry_run=dry_run)
+    flywheel_proxy = FlywheelProxy(api_key=api_key, dry_run=dry_run)
+
+    admin_users = get_admin_users(flywheel_proxy=flywheel_proxy,
+                                  group_name=admin_group_name)
+
+    run(proxy=flywheel_proxy,
+        project_list=project_list,
+        admin_users=admin_users)
 
 
 if __name__ == "__main__":
