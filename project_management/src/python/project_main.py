51839e03afc6f10f624829ce1e8863905d53dd19
Ben Keller <bjkeller@vt.edu>
2023-03-17 22:02:11 +0000

    Add template project parameter

--- project_management/src/python/project_main.py
+++ project_management/src/python/project_main.py
@@ -1,26 +1,27 @@
 """Defines project management computation."""
 
 import logging
-from typing import List, Optional
+from typing import Dict, List
 
 import flywheel
 from projects.flywheel_proxy import FlywheelProxy
 from projects.project import Project
 from projects.project_mapping import ProjectMappingAdaptor
+from projects.template_project import TemplateProject
 
 log = logging.getLogger(__name__)
 
 
 def run(*, proxy: FlywheelProxy, project_list,
         admin_users: List[flywheel.User],
-        gear_rules: Optional[object]):
-    # TODO: GEAR RULE - fix type of gear_rules in arguments
+        template_map: Dict[str, Dict[str, TemplateProject]]):
     """Runs project pipeline creation/management.
 
     Args:
       proxy: the proxy for the Flywheel instance
       project_list: the list of project input
       admin_users: the list of admin users
+      template_map: map from datatype name to template projects
     """
 
     for project_doc in project_list:
@@ -28,5 +29,5 @@ def run(*, proxy: FlywheelProxy, project_list,
         project_mapper = ProjectMappingAdaptor(project=project,
                                                flywheel_proxy=proxy,
                                                admin_users=admin_users,
-                                               gear_rules=gear_rules)
+                                               template_map=template_map)
         project_mapper.create_project_pipelines()
