d7eefd8f41c64e25da49a36b0008472ae3c50431
Ben Keller <bjkeller@vt.edu>
2023-03-27 20:42:34 +0000

    Factor out ability to get environment variable value

--- common/src/python/inputs/environment.py
+++ common/src/python/inputs/environment.py
@@ -3,13 +3,27 @@ import os
 from typing import Optional
 
 
+def get_environment_variable(name: str) -> Optional[str]:
+    """Gets the value of the environment variable.
+
+    Note: converts the variable name to upper case.
+
+    Returns:
+      The value of the environment variable.
+    """
+    value = None
+    variable = name.upper()
+    if variable in os.environ:
+        value = os.environ[variable]
+    return value
+
+
 def get_api_key() -> Optional[str]:
     """Get the Flywheel API key from environment.
 
     Returns:
         the API Key if defined. None, otherwise.
     """
-    api_key = None
-    if 'FW_API_KEY' in os.environ:
-        api_key = os.environ['FW_API_KEY']
-    return api_key
+    return get_environment_variable('FW_API_KEY')
+
+
