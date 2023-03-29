0bd493c5477c8fd42a86f8e2867213fe3f0fe6eb
Ben Keller <bjkeller@vt.edu>
2023-03-29 22:27:28 +0000

    Refactor run script

--- directory_pull/src/python/run.py
+++ directory_pull/src/python/run.py
@@ -2,9 +2,16 @@
 user management gear."""
 import logging
 import sys
-import yaml
+from typing import Any, Dict, List
 
+import flywheel
+import yaml
+from flywheel import FileSpec
+from flywheel_gear_toolkit import GearToolkitContext
+from inputs.arguments import build_parser_with_output
+from inputs.context_parser import parse_config
 from inputs.environment import get_api_key, get_environment_variable
+from projects.flywheel_proxy import FlywheelProxy
 from redcap.nacc_directory import UserDirectoryEntry
 from redcap.redcap_connection import REDCapConnection, REDCapConnectionError
 
@@ -12,6 +19,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
+# pylint: disable=(too-many-locals)
 def main() -> None:
     """Main method for directory pull.
 
@@ -19,6 +27,24 @@ def main() -> None:
     from directory on REDCap, and api key for flywheel. These must be
     given as environment variables.
     """
+
+    parser = build_parser_with_output()
+    args = parser.parse_args()
+
+    if args.gear:
+        filename = 'user_file'
+        with GearToolkitContext() as gear_context:
+            gear_context.init_logging()
+            context_args = parse_config(gear_context=gear_context,
+                                        filename=filename)
+            admin_group_name = context_args['admin_group']
+            dry_run = context_args['dry_run']
+            user_filename = context_args[filename]
+    else:
+        dry_run = args.dry_run
+        user_filename = args.filename
+        admin_group_name = args.admin_group
+
     api_key = get_api_key()
     if not api_key:
         log.error('No API key: expecting FW_API_KEY to be set')
@@ -42,35 +68,92 @@ def main() -> None:
         log.error('No report ID: expecting %s to be set', report_id_variable)
         sys.exit(1)
 
-    directory_proxy = REDCapConnection(token=directory_token,
-                                       url=directory_url)
-
+    user_entries = []
     try:
-        user_report = directory_proxy.request_json_value(
-            data={
-                'token': directory_token,
-                'content': 'report',
-                'format': 'json',
-                'report_id': str(user_report_id),
-                'csvDelimiter': '',
-                'rawOrLabel': 'raw',
-                'rawOrLabelHeaders': 'raw',
-                'exportCheckboxLabel': 'false',
-                'returnFormat': 'json'
-            },
-            message="pulling user report from NACC Directory")
+        user_entries = get_user_records(directory_token=directory_token,
+                                        directory_url=directory_url,
+                                        user_report_id=user_report_id)
     except REDCapConnectionError as error:
         log.error('Failed to pull users from directory: %s', error.error)
         sys.exit(1)
 
-    # Convert records in directory report to UserDirectoryEntry and then
-    # write to a file  
+    if args.gear:
+        admin_project = get_admin_project(admin_group_name, dry_run, api_key)
+        if not admin_project:
+            sys.exit(1)
+
+        if dry_run:
+            log.info('Would write user entries to file %s on project %s',
+                     user_filename, admin_project.label)
+            return
+
+        admin_project.upload_file(
+            FileSpec(user_filename,
+                     contents=yaml.safe_dump(user_entries),
+                     content_type='text/yaml'))
+    else:
+        with open(user_filename, mode='w', encoding='utf-8') as user_file:
+            yaml.safe_dump(user_entries, user_file)
+
+
+def get_admin_project(admin_group_name: str, dry_run: bool,
+                      api_key: str) -> flywheel.Project:
+    """Gets the admin project from the admin group.
+
+    Args:
+      admin_group_name: the name of the admin group
+      dry_run: whether to do a dry run on FW operations
+      api_key: the FW API key
+    """
+    flywheel_proxy = FlywheelProxy(api_key=api_key, dry_run=dry_run)
+    admin_group = None
+    groups = flywheel_proxy.find_groups(admin_group_name)
+    if groups:
+        admin_group = groups[0]
+    else:
+        log.error("Admin group %s not found", admin_group_name)
+        return None
+
+    project_name = 'project-admin'
+    admin_project = flywheel_proxy.get_project(group=admin_group,
+                                               project_label=project_name)
+    if not admin_project:
+        log.error('Unable to access admin project: %s', project_name)
+        return None
+
+    return admin_project
+
+
+def get_user_records(*, directory_token: str, directory_url: str,
+                     user_report_id: str) -> List[Dict[str, Any]]:
+    """Convert records in directory user report to UserDirectoryEntry and save
+    as list of dictionary objects.
+
+    Args:
+      user_report: the list of user records from directory report
+    """
+
+    directory_proxy = REDCapConnection(token=directory_token,
+                                       url=directory_url)
+
+    user_report = directory_proxy.request_json_value(
+        data={
+            'content': 'report',
+            'report_id': str(user_report_id),
+            'csvDelimiter': '',
+            'rawOrLabel': 'raw',
+            'rawOrLabelHeaders': 'raw',
+            'exportCheckboxLabel': 'false'
+        },
+        message="pulling user report from NACC Directory")
+
     user_entries = []
     for user_record in user_report:
         entry = UserDirectoryEntry.create_from_record(user_record)
         if entry:
             user_entries.append(entry.as_dict())
+    return user_entries
 
-    yaml_entries = yaml.safe_dump(user_entries)
 
-    
+if __name__ == "__main__":
+    main()
