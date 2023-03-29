6acb26d7e0a7964db2e68cb0793ceb49ceb942c2
Ben Keller <bjkeller@vt.edu>
2023-03-29 22:26:22 +0000

    Refactor argument builder

--- common/src/python/inputs/arguments.py
+++ common/src/python/inputs/arguments.py
@@ -7,8 +7,23 @@ context.
 import argparse
 
 
-def build_parser():
+def build_parser_with_input() -> argparse.ArgumentParser:
     """Builds and returns the argument parser."""
+    parser = build_base_parser()
+    parser.add_argument('filename', help='path of input file')
+    return parser
+
+
+def build_parser_with_output() -> argparse.ArgumentParser:
+    """Builds and returns an argument parser with an output file."""
+    parser = build_base_parser()
+    parser.add_argument('filename', help='path for output file')
+    return parser
+
+
+def build_base_parser() -> argparse.ArgumentParser:
+    """Builds an argument parser with arguments to indicate running as a gear,
+    whether to do a dry run, and the name of the admin group."""
     parser = argparse.ArgumentParser(
         description="Create FW structures for Project")
     parser.add_argument('-g',
@@ -25,5 +40,5 @@ def build_parser():
                         '--admin_group',
                         help='Group ID for the admin group',
                         default='nacc')
-    parser.add_argument('filename', help='path of input file')
+
     return parser
