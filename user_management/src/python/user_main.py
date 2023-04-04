4cbdfa93e1a8954c5a2098ea60facf8280611dc5
Ben Keller <bjkeller@vt.edu>
2023-04-04 14:28:01 +0000

    Update main

--- user_management/src/python/user_main.py
+++ user_management/src/python/user_main.py
@@ -1,4 +1,5 @@
 """Run method for user management."""
+from collections import defaultdict
 import logging
 from typing import List
 
@@ -12,12 +13,27 @@ log = logging.getLogger(__name__)
 def run(*, proxy: FlywheelProxy, user_list, admin_users: List[flywheel.User]):
     """does the work."""
 
-    for user in admin_users:
-        log.info("User: %s", user)
+    # ignore administrative users in directory
+    admin_map = { user.id : user for user in admin_users }
 
-    # visitor = FlywheelProjectArtifactCreator(flywheel_proxy)
+    # gather users by center
+    center_prefix = 'adcid-'
+    center_map = defaultdict(list)
     for user_doc in user_list:
-        user = UserDirectoryEntry.create(user_doc)
-        # project = Project.create(user_doc)
-        # project.apply(visitor)
-        pass
+        user_entry = UserDirectoryEntry.create(user_doc)
+        if user_entry.credentials['id'] in admin_map:
+            log.info('Skipping admin user: %s', user_entry.credentials['id'])
+            continue
+        center_map[f"{center_prefix}{user_entry.center_id}"].append(user_entry)
+
+    for center_tag, center_users in center_map.items():
+        group_list = proxy.find_groups_by_tag(center_tag)
+        if len(group_list) > 1:
+            log.error('Error: expecting only one center for tag %s', center_tag)
+            continue
+        center_group = group_list[0]
+
+        for project in center_group.projects:
+            pass
+        
+
