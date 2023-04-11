a76f681cf292b45999bcece01f40bb73d7e4ac20
Ben Keller <bjkeller@vt.edu>
2023-04-11 18:28:05 +0000

    Change handling of accepted template

--- common/src/python/inputs/templates.py
+++ common/src/python/inputs/templates.py
@@ -28,12 +28,17 @@ def get_template_projects(
     """
     template_map: Dict[str, Dict[str, TemplateProject]] = defaultdict(dict)
     if group:
-        template_matcher = re.compile(r"^(\w+)-(\w+)-template$")
+        template_matcher = re.compile(r"^((\w+)-)?(\w+)-template$")
+        # group 2 datatype
+        # group 3 stage
         for project in group.projects():
             match = template_matcher.match(project.label)
             if match:
-                datatype = match.group(1)
-                stage = match.group(2)
+                datatype = match.group(2)
+                if not datatype:
+                    # accepted stage has no datatype, set to 'all'
+                    datatype = 'all'
+                stage = match.group(3)
 
                 # TODO: stage list needs to come from elsewhere
                 if stage not in ['accepted', 'ingest', 'retrospective']:
