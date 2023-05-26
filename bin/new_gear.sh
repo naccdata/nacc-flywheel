21638b140f02292351e9e57ba9c752e4b6e7210e
Ben Keller <bjkeller@vt.edu>
2023-05-26 17:51:21 +0000

    Update gear init script to use template files

--- bin/new_gear.sh
+++ bin/new_gear.sh
@@ -2,6 +2,8 @@
 
 echo "making new gear directories named '$1'"
 
+DIRNAME=$1
+IMAGENAME=`echo $1 | tr '_' '-'`
 
 mkdir -p $1/src/python
 touch $1/src/python/main.py
@@ -11,8 +13,9 @@ mkdir -p $1/test/python
 touch $1/test/python/.gitkeep
 
 mkdir -p $1/src/docker
-touch $1/src/docker/Dockerfile
-touch $1/src/docker/manifest.json
+cat templates/docker/build-template.txt | envsubst > $1/src/docker/BUILD
+cat templates/docker/dockerfile-template.txt | envsubst '${DIRNAME}' > $1/src/docker/Dockerfile
+cat templates/docker/manifest-template.txt | envsubst > $1/src/docker/manifest.json
 
 mkdir -p docs/$1
 touch docs/$1/index.md
