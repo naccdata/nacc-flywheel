01594620267fe0bb05a5cce1587d28fa2f2ba112
Ben Keller <bjkeller@vt.edu>
2023-05-26 16:38:55 -0700

    Template gear creation (#14)
    * Enable gear command goal
    
    * Update gear init script to use template files
    
    * update documentation about new gear config
    
    * Improve templating
    
    * Improve templating
    
    * Refine dev docs

--- bin/new_gear.sh
+++ bin/new_gear.sh
@@ -2,17 +2,27 @@
 
 echo "making new gear directories named '$1'"
 
+project=$1
+prefix="${project%%_*}"
+export DIRNAME=$1
+export IMAGENAME=`echo $1 | tr '_' '-'`
+export APPNAME="${prefix}_app"
+export FILEKEY="${prefix}_file"
+export MAINFILE="${prefix}_main"
+
 
 mkdir -p $1/src/python
-touch $1/src/python/main.py
-touch $1/src/python/run.py
+cat templates/python/build-template.txt | envsubst > $1/src/python/BUILD
+cat templates/python/run-template.txt | envsubst > $1/src/python/run.py
+cat templates/python/main-template.txt | envsubst > "$1/src/python/${MAINFILE}.py"
 
 mkdir -p $1/test/python
 touch $1/test/python/.gitkeep
 
 mkdir -p $1/src/docker
-touch $1/src/docker/Dockerfile
-touch $1/src/docker/manifest.json
+cat templates/docker/build-template.txt | envsubst > $1/src/docker/BUILD
+cat templates/docker/dockerfile-template.txt | envsubst '${DIRNAME}' > $1/src/docker/Dockerfile
+cat templates/docker/manifest-template.txt | envsubst > $1/src/docker/manifest.json
 
 mkdir -p docs/$1
 touch docs/$1/index.md
