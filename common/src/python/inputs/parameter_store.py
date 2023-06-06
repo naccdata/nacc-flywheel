7642adb1a318adc54939a27b7bf58fe9f16bab9d
Ben Keller <bjkeller@vt.edu>
2023-06-06 20:35:19 +0000

    Add code to get api key from parameter store

---
+++ common/src/python/inputs/parameter_store.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+
+from inputs.environment import get_environment_variable
+from ssm_parameter_store import EC2ParameterStore
+
+
+def get_parameter_store() -> Optional[EC2ParameterStore]:
+    """Gets a proxy object for the parameter store if AWS credentials are set.
+    Expects AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID, and AWS_DEFAULT_REGION.
+
+    Returns:
+        parameter store object if credentials are valid, and None otherwise
+    """
+    secret_key = get_environment_variable('AWS_SECRET_ACCESS_KEY')
+    access_id = get_environment_variable('AWS_ACCESS_KEY_ID')
+    region = get_environment_variable('AWS_DEFAULT_REGION')
+    if not secret_key or not access_id or not region:
+        return None
+
+    return EC2ParameterStore(aws_access_key_id=access_id,
+                             aws_secret_access_key=secret_key,
+                             region_name=region)
