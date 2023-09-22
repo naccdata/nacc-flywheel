1628a052d3306d16bd11bdc3da2923a9bc825bcb
Ben Keller <bjkeller@vt.edu>
2023-09-22 17:46:54 +0000

    Use gear context

--- pull_scan_metadata/src/python/scan_metadata_app/run.py
+++ pull_scan_metadata/src/python/scan_metadata_app/run.py
@@ -4,25 +4,13 @@
 import logging
 import sys
 
-# from flywheel_gear_toolkit import GearToolkitContext
+from flywheel_gear_toolkit import GearToolkitContext
 from scan_metadata_app.main import run
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-log = logging.getLogger(__name__)
 
 
 def main():
-    """
-    Uses command line argument `gear` to indicate whether being run as a gear.
-    If running as a gear, the arguments are taken from the gear context.
-    Otherwise, arguments are taken from the command line.
-    """
-    # parser = build_base_parser()
-    # args = parser.parse_args()
-
-    # if args.gear:
-    #     with GearToolkitContext() as gear_context:
-    #         gear_context.init_logging()
+    with GearToolkitContext() as gear_context:
+         gear_context.init_logging()
 
     run()
 
