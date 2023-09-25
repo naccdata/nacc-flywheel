ffbb1168fd44e9a6cdf503dd9149278a42901929
Ben Keller <bjkeller@vt.edu>
2023-09-25 22:46:53 +0000

    Refactor run and main for scan metadata
    Refactor run and main for scan metadata
    1. Move hardcoded values into arguments set in run
    2. Create class to split tables in both scenarios
    3. Add error handling and logging
    4. Catch config errors early in run

--- pull_scan_metadata/src/python/scan_metadata_app/main.py
+++ pull_scan_metadata/src/python/scan_metadata_app/main.py
@@ -3,136 +3,201 @@
 import logging
 import re
 from io import BytesIO
+from typing import Dict, List, Optional
 
-import boto3
 import pandas as pd
-from flywheel import FileSpec, Project
+from flywheel import FileSpec, Group
 from flywheel_adaptor.flywheel_proxy import FlywheelProxy
 
 log = logging.getLogger(__name__)
 
 
-def get_parameter(parameter_name) -> str:
-    # Get parameter from SSM Parameter Store
-    ssm_client = boto3.client('ssm', region_name='us-west-2')
-    response = ssm_client.get_parameter(Name=parameter_name,
-                                        WithDecryption=True)
-    parameter_value = response['Parameter']['Value']
-    return parameter_value
+class SCANTable:
+    """Wrapper for data frame for SCAN metadata table.
+
+    Supports splitting table by Center ID. Center ID could be in column
+    named ADCID or SITE.
+    """
+
+    def __init__(self, *, data: pd.DataFrame, site_id_column: str) -> None:
+        self.__data_table = data
+        self.__site_column = site_id_column
+
+    @classmethod
+    def create_from(cls, table_data: pd.DataFrame) -> Optional['SCANTable']:
+        """Recognizes which column is used for site ID.
+
+        Args:
+          table_data: the data frame with data
+        Returns:
+          a wrapper object for the data frame or None if no center id column
+        """
+        if 'ADCID' in table_data.columns:
+            site_id_name = 'ADCID'
+        elif 'SITE' in table_data.columns:
+            site_id_name = 'SITE'
+        else:
+            return None
+
+        return SCANTable(data=table_data, site_id_column=site_id_name)
+
+    def site_keys(self) -> Dict[str, Optional[str]]:
+        """Returns a map from site column values to the ADCID.
+
+        Note: it is possible that an ADCID may not be found, in which case
+        returns None.
+
+        Returns:
+          dictionary mapping from site column value to ADCID or None
+        """
+        site_ids = self.__data_table.loc(self.__site_column)
+        return {site_key: self.get_adcid(site_key) for site_key in site_ids}
+
+    def get_adcid(self, key) -> Optional[str]:
+        """Returns the ADCID for the site column ID value.
+
+        Args:
+          key: the column key value
+        Returns:
+          the corresponding ADCID or None if none
+        """
+        if self.__site_column == 'ADCID':
+            return key
+
+        match = re.search(r"([^(]+)\(ADC\s?(\d+)\)", key)
+        if not match:
+            return None
+        return match.group(2).strip()
+
+    def select_site(self, site_id) -> pd.DataFrame:
+        """Selects the rows of the table for the site.
+
+        Args:
+          site_id: the ID of the table to select
+        Returns:
+          Data frame with rows of the table for the site_id
+        """
+        return self.__data_table.loc[self.__data_table[self.__site_column] ==
+                                     site_id]
 
 
-def get_fw_proxy() -> FlywheelProxy:
-    api_key = get_parameter(parameter_name='/prod/flywheel/gearbot/apikey')
-    fw = FlywheelProxy(api_key=api_key)
-    return fw
+def read_data(*, s3_client, bucket_name: str, file_name: str):
+    """Reads the file object from S3 with bucket name and file name.
 
-
-def stream_file_to_container(fw_file: FileSpec, container: Project) -> None:
-    container.upload_file(fw_file)
-
-
-def upload_center_file(adcid, table_data: pd.DataFrame, table_name: str):
-    print(f"Uploading {table_name} for adcid {adcid}")
-
-    csv_data = table_data.to_csv(index=False)
-    fw_file = FileSpec(f"{table_name}.csv", csv_data)
-
-    fw = get_fw_proxy()
-
-    group = fw.find_groups_by_tag(tag_pattern=f"adcid-{adcid}")
-    project = fw.get_project(group=group[0], project_label="ingest-scan")
-
-    stream_file_to_container(fw_file=fw_file, container=project)
-    return
-
-
-def split_table(table_data, table_name):
-    """Splits the table data into center subsets and uploads them to a FW
-    project."""
-
-    if 'ADCID' in table_data.columns:
-        # unique_adcid = list(table_data['ADCID'].unique())
-        unique_adcid = [42]
-        for adcid in unique_adcid:
-            center_subset = table_data.loc[table_data['ADCID'] == adcid]
-
-            # Upload center subset to FW
-            upload_center_file(adcid, center_subset, table_name)
-            exit()
-
-    elif 'SITE' in table_data.columns:
-        # unique_adcid = table_data['SITE'].unique()
-        unique_adcid = ['Wake Forest University (ADC42)']
-        for adcid in unique_adcid:
-            adcid = re.search("([^(]+)\(ADC\s?(\d+)\)", adcid).group(2).strip()
-            center_subset = table_data[table_data['SITE'].str.contains(
-                f'\(ADC\s?{adcid}\)')]
-
-            # Upload center subset to FW
-            upload_center_file(adcid, center_subset, table_name)
-            exit()
-
-    else:
-        log.error(
-            f'{table_name} does not have a recognized column for the Center ID'
-        )
-
-
-def get_s3_client():
-    # Get S3 credentials
-    access_key = get_parameter(
-        parameter_name='/prod/flywheel/gearbot/loni/accesskey')
-    secret_key = get_parameter(
-        parameter_name='/prod/flywheel/gearbot/loni/secretkey')
-    region = 'us-west-2'
-
-    # Initialize the S3 client
-    client = boto3.client('s3',
-                          aws_access_key_id=access_key,
-                          aws_secret_access_key=secret_key,
-                          region_name=region)
-
-    return client
-
-
-def download_table(s3_client, file_name) -> pd.DataFrame:
-    bucket_name = 'loni-table-data'
-
-    # Download the object from S3
+    Args:
+      s3_client: client for S3
+      bucket_name: bucket prefix
+      file_name: name of file
+    """
     response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
-    object_data = response['Body'].read()
-
-    # Convert the object data to a Pandas DataFrame
-    data = pd.read_csv(BytesIO(object_data))
-
-    return data
+    return response['Body'].read()
 
 
-def run():
+def load_table(object_data) -> pd.DataFrame:
+    """Converts the data to a DataFrame.
+
+    Args:
+      object_data: CSV file object from S3
+    Returns:
+      DataFrame containing data
+    """
+    return pd.read_csv(BytesIO(object_data))
+
+
+def build_center_map(*, proxy: FlywheelProxy,
+                     center_tag_pattern: str) -> Dict[str, Group]:
+    """Builds a map from adcid to group.
+
+    Args:
+      center_tag_pattern:
+      proxy: the flywheel instance proxy
+    Returns:
+      dictionary mapping from adcid to group
+    """
+    group_list = proxy.find_groups_by_tag(center_tag_pattern)
+    if not group_list:
+        log.warning('no centers found matching tag pattern %s',
+                    center_tag_pattern)
+        return {}
+
+    center_map = {}
+    for group in group_list:
+        pattern = re.compile(center_tag_pattern)
+        tags = list(filter(pattern.match, group.tags))
+        for tag in tags:
+            center_map[tag] = group
+
+    return center_map
+
+
+def upload_file(*, project, site_table, file_name) -> None:
+    """Creates CSV file and uploads to Flywheel project.
+
+    Args:
+      project: the Flywheel project
+      site_table: the table for site data
+      file_name: the name of the file on Flywheel
+    """
+    csv_data = site_table.to_csv(index=False)
+    file_spec = FileSpec(name=file_name,
+                         contents=csv_data,
+                         content_type='text/csv')
+    project.upload_file(file_spec)
+
+
+# pylint: disable=(too-many-locals)
+def run(*, proxy: FlywheelProxy, table_list: List[str], s3_client,
+        center_tag_pattern: str, bucket_name: str,
+        destination_label: str) -> None:
     """Pulls SCAN metadata from S3, splits the data by center, and uploads the
     data to the center-specific FW project.
 
     Args:
-      flywheel_proxy: the proxy for the Flywheel instance
+      proxy: the proxy for the Flywheel instance
+      table_list: the list of metadata table names
+      s3_client: the S3 client for accessing files
+      center_tag_pattern: regex pattern to match center tags
     """
-    table_list = [
-        "v_scan_upload_with_qc", "v_scan_mri_dashboard", "v_scan_pet_dashboard"
-    ]
 
-    s3_client = get_s3_client()
-    try:
-        for table_name in table_list:
-            log.info(f"Downloading {table_name} from S3")
-            table_data = download_table(s3_client,
-                                        file_name=f"{table_name}.csv")
+    center_map = build_center_map(proxy=proxy,
+                                  center_tag_pattern=center_tag_pattern)
 
-            log.info(f"Splitting table {table_name}")
-            split_table(table_data, table_name)
+    for table_name in table_list:
+        log.info("Downloading %s from S3", table_name)
+        filename = f"{table_name}.csv"
+        try:
+            data = read_data(s3_client=s3_client,
+                             bucket_name=bucket_name,
+                             file_name=filename)
+        except s3_client.exceptions.NoSuchKey:
+            log.error('File %s not found in bucket %s', filename, bucket_name)
+            continue
 
-    except Exception as e:
-        log.error(f"{e}")
-        print(e)
+        table = SCANTable.create_from(load_table(data))
+        if not table:
+            log.error(
+                'Table %s does not have a column with recognized center ID',
+                table_name)
+            continue
 
+        log.info("Splitting table %s", table_name)
+        for site_key, adcid in table.site_keys():
+            if not adcid:
+                log.warning('Site %s has no matching ADCID', site_key)
+                continue
 
-# if __name__ == '__main__':
-#     run()
+            site_table = table.select_site(site_key)
+            center_group = center_map.get(f'adcid-{adcid}')
+            if not center_group:
+                log.error('Did not find group for ADCID %s', adcid)
+                continue
+
+            project = proxy.get_project(group=center_group,
+                                        project_label=destination_label)
+            if not project:
+                log.error('Unable to access project %s', destination_label)
+                continue
+
+            upload_file(project=project,
+                        site_table=site_table,
+                        file_name=filename)
--- pull_scan_metadata/src/python/scan_metadata_app/run.py
+++ pull_scan_metadata/src/python/scan_metadata_app/run.py
@@ -1,19 +1,84 @@
 # COPIED FROM ANOTHER PROJECT. MUST BE ALTERED FOR PULL SCAN PROJECT
-
 """Main function for running template push process."""
 import logging
 import sys
 
+import boto3
+from flywheel import Client
+from flywheel_adaptor.flywheel_proxy import FlywheelProxy
 from flywheel_gear_toolkit import GearToolkitContext
+from inputs.api_key import get_api_key
+from inputs.context_parser import parse_config
+from inputs.parameter_store import get_parameter_store
 from scan_metadata_app.main import run
+from ssm_parameter_store import EC2ParameterStore
+
+log = logging.getLogger(__name__)
 
 
 def main():
-    with GearToolkitContext() as gear_context:
-         gear_context.init_logging()
+    """Main method to distribute SCAN metadata from S3 bucket to center
+    projects."""
 
-    run()
+    with GearToolkitContext() as gear_context:
+        gear_context.init_logging()
+        context_args = parse_config(gear_context=gear_context, filename=None)
+        dry_run = context_args['dry_run']
+
+    parameter_store = get_parameter_store()
+    if not parameter_store:
+        log.error('Unable to connect to parameter store')
+        sys.exit(1)
+
+    api_key = get_api_key(parameter_store)
+    if not api_key:
+        log.error('No API key found. Check API key configuration')
+        sys.exit(1)
+
+    proxy = FlywheelProxy(client=Client(api_key), dry_run=dry_run)
+
+    s3_client = get_s3_client(store=parameter_store,
+                              path='/prod/flywheel/gearbot/loni')
+    if not s3_client:
+        log.error('Unable to connect to S3')
+        sys.exit(1)
+
+    table_list = [
+        "v_scan_upload_with_qc", "v_scan_mri_dashboard", "v_scan_pet_dashboard"
+    ]
+
+    run(proxy=proxy,
+        table_list=table_list,
+        s3_client=s3_client,
+        bucket_name='loni-table-data',
+        center_tag_pattern=r'adcid-\d+',
+        destination_label='ingest-scan')
 
 
 if __name__ == "__main__":
     main()
+
+
+def get_s3_client(*, store: EC2ParameterStore, path: str):
+    """Returns the S3 client for the access credentials in the parameter store
+    at the given path.
+
+    Args:
+      store: the parameter store with the credentials
+      path: the parameter store path for the S3 credentials
+    Returns:
+      the boto3 S3 client
+    """
+    # Get S3 credentials
+    parameters = store.get_parameters_by_path(path=path)
+    access_key = parameters.get('accesskey')
+    secret_key = parameters.get('secretkey')
+    region = parameters.get('region')
+
+    # Initialize the S3 client
+    client = boto3.client('s3',
+                          aws_access_key_id=access_key,
+                          aws_secret_access_key=secret_key,
+                          region_name=region)
+
+    return client
