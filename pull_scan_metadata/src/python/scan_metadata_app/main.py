bce953a98ee8032ead6d64a8419c4018a9cbe834
Ben Keller <bjkeller@vt.edu>
2023-09-26 19:47:54 +0000

    Update main to match other changes

--- pull_scan_metadata/src/python/scan_metadata_app/main.py
+++ pull_scan_metadata/src/python/scan_metadata_app/main.py
@@ -1,107 +1,26 @@
 """Pulls SCAN metadata from LONI."""
 
 import logging
-import re
-from typing import Dict, List, Optional
+from typing import Dict, List
 
 from flywheel import Project
-from flywheel_adaptor.flywheel_proxy import FlywheelProxy
+from s3.s3_client import read_data
 from tabular_data.site_table import SiteTable, upload_split_table
 
 log = logging.getLogger(__name__)
 
 
-def read_data(*, s3_client, bucket_name: str, file_name: str):
-    """Reads the file object from S3 with bucket name and file name.
+def run(*, table_list: List[str], s3_client, bucket_name: str,
+        project_map: Dict[str, Project]) -> None:
+    """Pulls tabular data from S3, splits the data by center, and uploads the
+    data to the center-specific FW project indicated by the project map.
 
     Args:
-      s3_client: client for S3
-      bucket_name: bucket prefix
-      file_name: name of file
-    """
-    response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
-    return response['Body'].read()
-
-
-def build_project_map(*, proxy: FlywheelProxy, center_tag_pattern: str,
-                      destination_label: str) -> Dict[str, Project]:
-    """Builds a map from adcid to the project of center group with the given
-    label.
-
-    Args:
-      proxy: the flywheel instance proxy
-      center_tag_pattern: the regex for adcid-tags
-      destination_label: the project of center to map to
-    Returns:
-      dictionary mapping from adcid to group
-    """
-    group_list = proxy.find_groups_by_tag(center_tag_pattern)
-    if not group_list:
-        log.warning('no centers found matching tag pattern %s',
-                    center_tag_pattern)
-        return {}
-
-    project_map = {}
-    for group in group_list:
-        project = proxy.get_project(group=group,
-                                    project_label=destination_label)
-        if not project:
-            continue
-
-        pattern = re.compile(center_tag_pattern)
-        tags = list(filter(pattern.match, group.tags))
-        for tag in tags:
-            project_map[tag] = project
-
-    return project_map
-
-
-def build_site_map(*, site_map: Dict[str, Optional[str]],
-                   project_map: Dict[str, Project]) -> Dict[str, Project]:
-    """Composes the site and project maps to create mapping that can be used to
-    split a site table and upload partitions to projects.
-
-    Args:
-      proxy: the proxy object for Flywheel instance
-      site_map: the map from site keys to adcids
-      center_map: the map from adcid tags to center groups
-      filename: the name of the file to use
-      destination_label: the name of the project in center group
-    """
-    project_map = {}
-    for site_key, adcid in site_map.items():
-        if not adcid:
-            log.warning('Site %s has no matching ADCID', site_key)
-            continue
-
-        project = project_map.get(f'adcid-{adcid}')
-        if not project:
-            log.error('Did not find project for ADCID %s', adcid)
-            continue
-
-        project_map[site_key] = project
-
-    return project_map
-
-
-def run(*, proxy: FlywheelProxy, table_list: List[str], s3_client,
-        center_tag_pattern: str, bucket_name: str,
-        destination_label: str) -> None:
-    """Pulls SCAN metadata from S3, splits the data by center, and uploads the
-    data to the center-specific FW project.
-
-    Args:
-      proxy: the proxy for the Flywheel instance
       table_list: the list of metadata table names
       s3_client: the S3 client for accessing files
-      center_tag_pattern: regex pattern to match center tags
       bucket_name: name of the source bucket
-      destination_label: label for destination project w/in each center group
+      project_map: map from ADCID to FW project for upload
     """
-
-    project_map = build_project_map(proxy=proxy,
-                                   center_tag_pattern=center_tag_pattern,
-                                   destination_label=destination_label)
 
     for table_name in table_list:
         log.info("Downloading %s from S3", table_name)
@@ -124,11 +43,14 @@ def run(*, proxy: FlywheelProxy, table_list: List[str], s3_client,
                 table_name)
             continue
 
+        # remap projects from ADCID instead of center tag
+        # TODO: need to abstract tag format
+        upload_map = {
+            adcid: project_map.get(f'adcid-{adcid}')
+            for adcid in table.get_adcids()
+        }
+
         log.info("Splitting table %s", table_name)
-        project_map = build_site_map(
-            site_map=table.site_keys(),
-            project_map=project_map,
-        )
         upload_split_table(table=table,
-                           project_map=project_map,
+                           project_map=upload_map,
                            file_name=filename)
