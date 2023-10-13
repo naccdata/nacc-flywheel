3481251ae6d345e87056f6c47e64d888e0031384
Ben Keller <bjkeller@vt.edu>
2023-10-13 21:19:49 +0000

    Refactor user creation

--- user_management/src/python/user_app/main.py
+++ user_management/src/python/user_app/main.py
@@ -1,18 +1,18 @@
 """Run method for user management."""
 import logging
 from collections import defaultdict
-from typing import Set, TypedDict
+from typing import Dict, List, Set
 
 from flywheel import RolesRoleAssignment, User
 from flywheel_adaptor.flywheel_proxy import FlywheelProxy
 from flywheel_adaptor.group_adaptor import GroupAdaptor
 from flywheel_adaptor.project_adaptor import ProjectAdaptor
-from redcap.nacc_directory import Authorizations, UserDirectoryEntry
+from redcap.nacc_directory import UserDirectoryEntry
 
 log = logging.getLogger(__name__)
 
 
-def create_user(proxy: FlywheelProxy, user_entry: UserDirectoryEntry) -> User:
+def create_user(proxy: FlywheelProxy, user_entry: UserDirectoryEntry) -> str:
     """Creates a user object from the directory entry.
 
     Flywheel constraints (true as of version 17):
@@ -23,29 +23,43 @@ def create_user(proxy: FlywheelProxy, user_entry: UserDirectoryEntry) -> User:
       proxy: the proxy object for the FW instance
       user_entry: the directory entry for the user
     Returns:
-      the flywheel User with the ID in directory entry
+      the ID for flywheel User created from the directory entry
     """
 
     user_id = user_entry.credentials['id'].lower()
     user = proxy.find_user(user_id)
     if user:
-        return user
+        return user_id
 
     new_id = proxy.add_user(
         User(id=user_id,
              firstname=user_entry.name['first_name'],
              lastname=user_entry.name['last_name'],
              email=user_id))
-    user = proxy.find_user(new_id)
-    assert user, f"Just created user {new_id}, should exist but is None"
-
-    return user
+    return new_id
 
 
-class UserAuthorizations(TypedDict):
-    """User-authorizations pair for."""
-    user: User
-    authorizations: Authorizations
+def create_user_map(
+        user_list, skip_list: Set[str]) -> Dict[str, List[UserDirectoryEntry]]:
+    """Creates a map from center tags to lists of nacc directory entries.
+
+    Args:
+      user_list: the list of user objects from directory yaml file
+      skip_list: the list of user IDs to skip
+    Returns:
+      map from center tags to lists of nacc directory entries
+    """
+    center_prefix = 'adcid-'
+    center_map = defaultdict(list)
+    for user_doc in user_list:
+        user_entry = UserDirectoryEntry.create(user_doc)
+        if user_entry.credentials['id'] in skip_list:
+            log.info('Skipping user: %s', user_entry.credentials['id'])
+            continue
+
+        center_map[f"{center_prefix}{user_entry.center_id}"].append(user_entry)
+
+    return center_map
 
 
 # pylint: disable=(too-many-locals)
@@ -53,18 +67,7 @@ def run(*, proxy: FlywheelProxy, user_list, skip_list: Set[str]):
     """Manages users based on user list."""
 
     # gather users by center
-    center_prefix = 'adcid-'
-    center_map = defaultdict(list)
-    for user_doc in user_list:
-        user_entry = UserDirectoryEntry.create(user_doc)
-        if user_entry.credentials['id'] in skip_list:
-            log.info('Skipping admin user: %s', user_entry.credentials['id'])
-            continue
-
-        user = create_user(proxy=proxy, user_entry=user_entry)
-        center_map[f"{center_prefix}{user_entry.center_id}"].append(
-            UserAuthorizations(user=user,
-                               authorizations=user_entry.authorizations))
+    center_map = create_user_map(user_list=user_list, skip_list=skip_list)
 
     roles_map = proxy.get_roles()
     read_only_role = roles_map.get('read-only')
@@ -89,7 +92,11 @@ def run(*, proxy: FlywheelProxy, user_list, skip_list: Set[str]):
             continue
 
         project_adaptor = ProjectAdaptor(project=project, proxy=proxy)
-        for user_authorization in center_users:
+        for user_entry in center_users:
+            user_id = create_user(proxy=proxy, user_entry=user_entry)
+            log.info('Added user %s', user_id)
+            log.info('Granting %s permission to user %s in project %s/%s',
+                     read_only_role.label, user_id, center_group.label,
+                     project_adaptor.label)
             project_adaptor.add_user_roles(
-                RolesRoleAssignment(id=user_authorization['user'].id,
-                                    role_ids=[read_only_role.id]))
+                RolesRoleAssignment(id=user_id, role_ids=[read_only_role.id]))
