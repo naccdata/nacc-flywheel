289056fbc05f11f45f425602216b2139e6e56a9f
Ben Keller <bjkeller@vt.edu>
2023-10-13 16:54:36 +0000

    Complete user management main
    Complete user management main:
    1. Add user creation from directory entry
    2. Add user permissions specific to SCAN dashboard demo

--- user_management/src/python/user_app/main.py
+++ user_management/src/python/user_app/main.py
@@ -1,31 +1,75 @@
 """Run method for user management."""
 import logging
 from collections import defaultdict
-from typing import List
+from typing import List, Set, TypedDict
 
-from flywheel import User
+from flywheel import RolesRoleAssignment, User
 from flywheel_adaptor.flywheel_proxy import FlywheelProxy
-from redcap.nacc_directory import UserDirectoryEntry
+from flywheel_adaptor.group_adaptor import GroupAdaptor
+from flywheel_adaptor.project_adaptor import ProjectAdaptor
+from redcap.nacc_directory import Authorizations, UserDirectoryEntry
 
 log = logging.getLogger(__name__)
 
 
-def run(*, proxy: FlywheelProxy, user_list, admin_users: List[User]):
-    """does the work."""
+def create_user(proxy: FlywheelProxy, user_entry: UserDirectoryEntry) -> User:
+    """Creates a user object from the directory entry.
 
-    # ignore administrative users in directory
-    admin_map = {user.id: user for user in admin_users}
+    Flywheel constraints (true as of version 17):
+    1. user IDs should be lowercase b/c FW treats them as case sensitive.
+    2. the user ID and email have to be the same even if ID is an ePPN in add_user
+
+    Args:
+      proxy: the proxy object for the FW instance
+      user_entry: the directory entry for the user
+    Returns:
+      the flywheel User with the ID in directory entry
+    """
+
+    user_id = user_entry.credentials['id'].lower()
+    user = proxy.find_user(user_id)
+    if user:
+        return user
+
+    new_id = proxy.add_user(
+        User(id=user_id,
+             firstname=user_entry.name['first_name'],
+             lastname=user_entry.name['last_name'],
+             email=user_id))
+    user = proxy.find_user(new_id)
+    assert user, f"Just created user {new_id}, should exist but is None"
+
+    return user
+
+
+class UserAuthorizations(TypedDict):
+    """User-authorizations pair for."""
+    user: User
+    authorizations: Authorizations
+
+
+def run(*, proxy: FlywheelProxy, user_list, skip_list: Set[str]):
+    """Manages users based on user list."""
 
     # gather users by center
     center_prefix = 'adcid-'
     center_map = defaultdict(list)
     for user_doc in user_list:
         user_entry = UserDirectoryEntry.create(user_doc)
-        if user_entry.credentials['id'] in admin_map:
+        if user_entry.credentials['id'] in skip_list:
             log.info('Skipping admin user: %s', user_entry.credentials['id'])
             continue
-        center_map[f"{center_prefix}{user_entry.center_id}"].append(user_entry)
-        user = User(id=user_entry.credentials['id'], firstname=user_entry.name['first_name'], lastname=user_entry.name['last_name'], email=user_entry.email)
+
+        user = create_user(proxy=proxy, user_entry=user_entry)
+        center_map[f"{center_prefix}{user_entry.center_id}"].append(
+            UserAuthorizations(user=user,
+                               authorizations=user_entry.authorizations))
+
+    roles_map = proxy.get_roles()
+    read_only_role = roles_map.get('read-only')
+    if not read_only_role:
+        log.error('Could not find read-only role, cannot add permissions')
+        return
 
     for center_tag, center_users in center_map.items():
         group_list = proxy.find_groups_by_tag(center_tag)
@@ -33,7 +77,18 @@ def run(*, proxy: FlywheelProxy, user_list, admin_users: List[User]):
             log.error('Error: expecting only one center for tag %s',
                       center_tag)
             continue
-        center_group = group_list[0]
+        center_group = GroupAdaptor(group=group_list[0], proxy=proxy)
 
-        for project in center_group.projects():
-            pass
+        # for now, just giving all users read-only access to 'ingest-scan'
+        project_label = 'ingest-scan'
+        project = center_group.get_project(project_label)
+        if not project:
+            log.warning('center %s does not have project %s',
+                        center_group.label, project_label)
+            continue
+
+        project_adaptor = ProjectAdaptor(project=project, proxy=proxy)
+        for user_authorization in center_users:
+            project_adaptor.add_user_roles(
+                RolesRoleAssignment(id=user_authorization.user.id,
+                                    role_ids=[read_only_role.id]))
