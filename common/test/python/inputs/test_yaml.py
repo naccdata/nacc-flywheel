1f1e47d4435236ade1cbd340749124b016739722
Ben Keller <bjkeller@vt.edu>
2023-10-14 17:43:55 +0000

    Add test for yaml input

---
+++ common/test/python/inputs/test_yaml.py
@@ -0,0 +1,19 @@
+"""Tests for handling of YAML documents."""
+import yaml
+from inputs.yaml import get_object_lists_from_stream
+
+
+class TestYAML:
+    """Test for handling of YAML documents."""
+
+    def test_empty(self):
+        """Test for empty list.
+
+        Function called returns list of lists.
+        """
+        empty_list = []
+        yaml_object = yaml.safe_dump(empty_list,
+                                     allow_unicode=True,
+                                     default_flow_style=False)
+        object_list = get_object_lists_from_stream(yaml_object, 'empty-list')
+        assert object_list == [[]]
