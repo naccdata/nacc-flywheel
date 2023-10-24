d5626c06bdab1422e5f9c5976e94c9bdf6f815ac
Ben Keller <bjkeller@vt.edu>
2023-10-24 14:25:02 +0000

    Check for user ID conflicts

--- pull_directory/src/python/directory_app/main.py
+++ pull_directory/src/python/directory_app/main.py
@@ -1,6 +1,7 @@
 """Module for handling user data from directory."""
 import logging
-from typing import Dict, List
+from collections import defaultdict
+from typing import Any, Dict, List
 
 import yaml
 from flywheel import FileSpec, Project
@@ -25,15 +26,32 @@ def run(*, user_report: List[Dict[str, str]], user_filename: str,
                  user_filename, project.label)
         return
 
-    user_entries = []
+    entry_map: Dict[str, Any] = {}
+    conflicts: Dict[str, List[Any]] = defaultdict(list)
     for user_record in user_report:
         entry = UserDirectoryEntry.create_from_record(user_record)
-        if entry:
-            user_entries.append(entry.as_dict())
+
+        if not entry:
+            continue
+
+        user_id = entry.credentials['id']
+        if user_id not in entry_map.keys():
+            entry_map[user_id] = entry.as_dict()
+            continue
+
+        conflicts[user_id].append(entry.as_dict())
+        conflicts[user_id].append(entry_map.pop(user_id))
 
     project.upload_file(
         FileSpec(user_filename,
-                 contents=yaml.safe_dump(user_entries,
+                 contents=yaml.safe_dump(list(entry_map.values()),
+                                         allow_unicode=True,
+                                         default_flow_style=False),
+                 content_type='text/yaml'))
+
+    project.upload_file(
+        FileSpec(f"conflicts-{user_filename}",
+                 contents=yaml.safe_dump(conflicts,
                                          allow_unicode=True,
                                          default_flow_style=False),
                  content_type='text/yaml'))
