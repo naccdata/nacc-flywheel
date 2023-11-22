caecb2375bbc41cd161816c5d15819f68fec044e
Ben Keller <bjkeller@vt.edu>
2023-11-22 18:29:50 +0000

    Remove parse_config
    Remove parse_config
    1. remove parse config function
    2. add function to get api from context

--- common/src/python/inputs/context_parser.py
+++ common/src/python/inputs/context_parser.py
@@ -3,19 +3,16 @@ from typing import Optional
 
 from flywheel_gear_toolkit import GearToolkitContext  # type: ignore
 
+def get_api_key(gear_context: GearToolkitContext) -> Optional[str]:
+    """Returns the api key from the gear context if there is one.
 
-def parse_config(*,
-                 gear_context: GearToolkitContext,
-                 filename: Optional[str] = None):
-    """Parses gear config for inputs."""
-    args = {}
-    args['dry_run'] = gear_context.config.get("dry_run", False)
-    args['new_only'] = gear_context.config.get("new_only", False)
-    if filename:
-        args[filename] = gear_context.get_input_path(filename)
-    args['admin_group'] = gear_context.config.get('admin_group', 'nacc')
+    Args:
+      gear_context: the gear context
+    Returns:
+      the API key if exists in context, None otherwise
+    """
     api_key_dict = gear_context.get_input('api-key')
-    if api_key_dict:
-        args['api_key'] = api_key_dict.get('key')
-
-    return args
+    if not api_key_dict:
+        return None
+    
+    return api_key_dict.get('key')
