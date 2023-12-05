e257edcaee5e25f2b175c7c8bf51e931cb8d67c8
Ben Keller <bjkeller@vt.edu>
2023-12-05 16:44:20 +0000

    Add gear prefix to paths

--- bin/new_gear.sh
+++ bin/new_gear.sh
@@ -12,20 +12,20 @@ export FILEKEY="${prefix}_file"
 export MAINFILE="${prefix}_main"
 
 
-mkdir -p $1/src/python/${APPNAME}
-cat templates/python/build-template.txt | envsubst > "$1/src/python/${APPNAME}/BUILD"
-cat templates/python/run-template.txt | envsubst > "$1/src/python/${APPNAME}/run.py"
-cat templates/python/main-template.txt | envsubst > "$1/src/python/${APPNAME}/main.py"
-touch "$1/src/python/${APPNAME}/__init__.py"
+mkdir -p gear/$1/src/python/${APPNAME}
+cat templates/python/build-template.txt | envsubst > "gear/$1/src/python/${APPNAME}/BUILD"
+cat templates/python/run-template.txt | envsubst > "gear/$1/src/python/${APPNAME}/run.py"
+cat templates/python/main-template.txt | envsubst > "gear/$1/src/python/${APPNAME}/main.py"
+touch "gear/$1/src/python/${APPNAME}/__init__.py"
 
-mkdir -p $1/test/python
-touch $1/test/python/.gitkeep
+mkdir -p gear/$1/test/python
+touch gear/$1/test/python/.gitkeep
 
-mkdir -p $1/src/docker
-cat templates/docker/build-template.txt | envsubst > $1/src/docker/BUILD
-cat templates/docker/dockerfile-template.txt | envsubst '${DIRNAME}' > $1/src/docker/Dockerfile
-cat templates/docker/manifest-template.txt | envsubst > $1/src/docker/manifest.json
-cat templates/docker/gitignore-template.txt > $1/src/docker/.gitignore
+mkdir -p gear/$1/src/docker
+cat templates/docker/build-template.txt | envsubst > gear/$1/src/docker/BUILD
+cat templates/docker/dockerfile-template.txt | envsubst '${DIRNAME}' > gear/$1/src/docker/Dockerfile
+cat templates/docker/manifest-template.txt | envsubst > gear/$1/src/docker/manifest.json
+cat templates/docker/gitignore-template.txt > gear/$1/src/docker/.gitignore
 
 mkdir -p docs/$1
 touch docs/$1/index.md
