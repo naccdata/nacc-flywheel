b15f4f6117bfe16e6977f3f9e072f6627141c951
Ben Keller <bjkeller@vt.edu>
2023-12-12 22:46:47 +0000

    Add initial gear methods

--- gear/identifer_lookup/src/python/identifer_app/main.py
+++ gear/identifer_lookup/src/python/identifer_app/main.py
@@ -1,18 +1,25 @@
 """Defines ADD DETAIL computation."""
 
 import logging
-from typing import Any, List
+from typing import Any, Dict, List, TextIO
 
 from flywheel_adaptor.flywheel_proxy import FlywheelProxy
+from identifiers.model import Identifier
 
 log = logging.getLogger(__name__)
 
-def run(*,
-        proxy: FlywheelProxy,
-        file):
+
+def run(*, proxy: FlywheelProxy, input_file: TextIO,
+        identifiers: Dict[str, Identifier], output_file: TextIO,
+        error_file: TextIO) -> bool:
     """Runs ADD DETAIL process.
-    
+
     Args:
       proxy: the proxy for the Flywheel instance
+      input_file: the data input stream
+      output_file: the data output stream
+      error_file: the error output stream
+    Returns:
+      True if there were IDs with no corresponding NACCID
     """
-    pass
+    return False
--- gear/identifer_lookup/src/python/identifer_app/run.py
+++ gear/identifer_lookup/src/python/identifer_app/run.py
@@ -1,28 +1,30 @@
-"""ADD DETAIL HERE"""
+"""ADD DETAIL HERE."""
 
 import logging
+import re
 import sys
+
 from flywheel import Client
 from flywheel_adaptor.flywheel_proxy import FlywheelProxy
-
 from flywheel_gear_toolkit import GearToolkitContext
-from inputs.context_parser import ConfigParseError, get_config
 from identifer_app.main import run
+from identifiers.identifiers_repository import IdentifierRepository
+from inputs.context_parser import ConfigParseError, get_config
 from inputs.parameter_store import ParameterError, ParameterStore
-
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger(__name__)
 
+
 def main():
-    """Describe gear detail here"""
+    """Describe gear detail here."""
 
     with GearToolkitContext() as gear_context:
         gear_context.init_logging()
 
         try:
             rds_param_path = get_config(gear_context=gear_context,
-                                       key='rds_parameter_path')
+                                        key='rds_parameter_path')
         except ConfigParseError as error:
             log.error('Incomplete configuration: %s', error.message)
             sys.exit(1)
@@ -30,7 +32,8 @@ def main():
         try:
             parameter_store = ParameterStore.create_from_environment()
             api_key = parameter_store.get_api_key()
-            rds_parameters = parameter_store.get_rds_parameters(param_path=rds_param_path)
+            rds_parameters = parameter_store.get_rds_parameters(
+                param_path=rds_param_path)
         except ParameterError as error:
             log.error('Parameter error: %s', error)
             sys.exit(1)
@@ -38,14 +41,47 @@ def main():
         dry_run = gear_context.config.get("dry_run", False)
         proxy = FlywheelProxy(client=Client(api_key), dry_run=dry_run)
 
-        # TODO: check whehter there is a method to open this file in context
-        input_file = gear_context.get_input_path('input_file')
+        file_input = gear_context.get_input('input_file')
+        if not file_input:
+            log.error('Missing input file')
+            sys.exit(1)
 
-    errors = run(proxy=proxy,
-        file=input_file)
-    
-    # TODO: check this applies to correct file object
-    gear_context.metadata.add_qc_result(input_file, "valid_identifiers", "FAIL" if errors else "PASS" )
+        # get the ADCID
+        parent = gear_context.get_container_from_ref(file_input['hierarchy'])
+        # TODO: make sure parent is a project
+        group = proxy.find_group(parent.group)
+        assert group
+        pattern = re.compile(r'adcid-(\d+)')
+        tag = list(filter(pattern.match, group.get_tags()))[0]
+        # TODO: if there is more than one it is a problem
+        match = pattern.match(tag)
+        if not match:
+            log.error('Unable to determine center ID')
+            sys.exit(1)
+
+        adcid = int(match.group(1))
+
+        identifiers_repo = IdentifierRepository.create_from(rds_parameters)
+        identifiers = {
+            identifier.ptid: identifier
+            for identifier in identifiers_repo.list(adc_id=adcid)
+        }
+
+        with gear_context.open_input(
+                'input_file', mode='r', encoding='utf-8') as csv_file, gear_context.open_output(
+                    'updated_input_file'
+                ) as out_file, gear_context.open_output(
+                    'error_file') as err_file:
+            errors = run(proxy=proxy,
+                         input_file=csv_file,
+                         identifiers=identifiers,
+                         output_file=out_file,
+                         error_file=err_file)
+
+            # TODO: check this applies to correct file object
+            gear_context.metadata.add_qc_result(input_file,
+                                                "valid_identifiers",
+                                                "FAIL" if errors else "PASS")
 
     if __name__ == "__main__":
         main()
