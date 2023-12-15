4947f28be19532b544ad3401ec769491b34f5050
Ben Keller <bjkeller@vt.edu>
2023-12-15 00:20:44 +0000

    Add non-error handling of input with tests

--- gear/identifer_lookup/src/python/identifer_app/main.py
+++ gear/identifer_lookup/src/python/identifer_app/main.py
@@ -1,17 +1,19 @@
 """Defines ADD DETAIL computation."""
 
+import json
 import logging
-from typing import Any, Dict, List, TextIO
+from csv import DictReader, DictWriter, Sniffer
+from typing import Dict, TextIO
 
-from flywheel_adaptor.flywheel_proxy import FlywheelProxy
 from identifiers.model import Identifier
+from outputs.errors import identifier_error
 
 log = logging.getLogger(__name__)
 
 
-def run(*, proxy: FlywheelProxy, input_file: TextIO,
-        identifiers: Dict[str, Identifier], output_file: TextIO,
-        error_file: TextIO) -> bool:
+    
+def run(*, input_file: TextIO, identifiers: Dict[str, Identifier],
+        output_file: TextIO, error_file: TextIO) -> bool:
     """Runs ADD DETAIL process.
 
     Args:
@@ -22,4 +24,41 @@ def run(*, proxy: FlywheelProxy, input_file: TextIO,
     Returns:
       True if there were IDs with no corresponding NACCID
     """
-    return False
+
+    sniffer = Sniffer()
+    csv_sample = input_file.read(1024)
+    if not csv_sample:
+        # TODO: output error for empty file
+        return True
+
+    if not sniffer.has_header(csv_sample):
+        # TODO: need error that no header
+        return True
+    input_file.seek(0)
+
+    detected_dialect = sniffer.sniff(csv_sample, delimiters=',')
+    reader = DictReader(input_file, dialect=detected_dialect)
+    assert reader.fieldnames
+
+    header_fields = list(reader.fieldnames)
+    if 'ptid' not in header_fields:
+        # TODO record missing ID column error
+        return True
+
+    header_fields.append('naccid')
+    writer = DictWriter(output_file, fieldnames=header_fields, dialect='unix')
+    writer.writeheader()
+
+    found_error = False
+    for record in reader:
+        assert record['ptid']
+        identifier = identifiers.get(record['ptid'])
+        if not identifier:
+            error = identifier_error(line=reader.line_num, value=record['ptid'])
+            found_error = True
+            continue
+
+        record['naccid'] = identifier.naccid
+        writer.writerow(record)
+
+    return found_error
---
+++ gear/identifer_lookup/test/python/test_identifer_app.py
@@ -0,0 +1,162 @@
+import csv
+from io import StringIO
+from typing import Any, List
+
+import pytest
+from identifer_app.main import run
+from identifiers.model import Identifier
+
+
+@pytest.fixture(scope="function")
+def empty_data_stream():
+    """Create empty data stream."""
+    yield StringIO()
+
+
+def write_to_stream(data: List[List[Any]], stream: StringIO) -> None:
+    writer = csv.writer(stream,
+                        delimiter=',',
+                        quotechar='\"',
+                        quoting=csv.QUOTE_NONNUMERIC,
+                        lineterminator='\n')
+    writer.writerows(data)
+
+
+@pytest.fixture(scope="function")
+def no_header_stream():
+    """Create data stream without header row."""
+    data = [[1, 1, 8], [1, 2, 99]]
+    stream = StringIO()
+    write_to_stream(data, stream)
+    stream.seek(0)
+    yield stream
+
+
+@pytest.fixture(scope="function")
+def no_ids_stream():
+    """Create data stream without expected column headers."""
+    data = [['dummy1', 'dummy2', 'dummy3'], [1, 1, 8], [1, 2, 99]]
+    stream = StringIO()
+    write_to_stream(data, stream)
+    stream.seek(0)
+    yield stream
+
+
+@pytest.fixture(scope="function")
+def data_stream():
+    """Create data stream without header row."""
+    data = [['adcid', 'ptid', 'var1'], [1, '1', 8], [1, '2', 99]]
+    stream = StringIO()
+    write_to_stream(data, stream)
+    stream.seek(0)
+    yield stream
+
+
+@pytest.fixture(scope="function")
+def identifiers_map():
+    """Create identifiers map with IDs matching data stream."""
+    id_map = {}
+    id_map['1'] = Identifier(nacc_id=1,
+                             adc_id=1,
+                             patient_id='1',
+                             nacc_adc=1111)
+    id_map['2'] = Identifier(nacc_id=2,
+                             adc_id=1,
+                             patient_id='2',
+                             nacc_adc=2222)
+    yield id_map
+
+
+@pytest.fixture(scope="function")
+def mismatched_identifiers_map():
+    """Create identifiers map with IDs that don't match data stream."""
+    id_map = {}
+    id_map['3'] = Identifier(nacc_id=1,
+                             adc_id=1,
+                             patient_id='3',
+                             nacc_adc=3333)
+    id_map['4'] = Identifier(nacc_id=2,
+                             adc_id=1,
+                             patient_id='4',
+                             nacc_adc=4444)
+    yield id_map
+
+
+def empty(stream) -> bool:
+    """Checks that the stream is empty."""
+    stream.seek(0)
+    return not bool(stream.readline())
+
+
+# pylint: disable=no-self-use,redefined-outer-name
+class TestIdentifierLookup:
+    """Tests for the identifier-lookup-gear app."""
+
+    def test_empty_input_stream(self, empty_data_stream: StringIO,
+                                identifiers_map: dict[Any, Any]):
+        """Test empty input stream."""
+        out_stream = StringIO()
+        err_stream = StringIO()
+        errors = run(input_file=empty_data_stream,
+                     identifiers=identifiers_map,
+                     output_file=out_stream,
+                     error_file=err_stream)
+        assert errors
+        assert empty(out_stream)
+
+        # TODO: add check for error output
+        # err_stream.seek(0)
+
+    def test_no_header(self, no_header_stream: StringIO, identifiers_map: dict[Any, Any]):
+        """Test case with no header."""
+        out_stream = StringIO()
+        err_stream = StringIO()
+        errors = run(input_file=no_header_stream,
+                     identifiers=identifiers_map,
+                     output_file=out_stream,
+                     error_file=err_stream)
+        assert errors
+
+    def test_no_id_column_headers(self, no_ids_stream: StringIO, identifiers_map: dict[Any, Any]):
+        """Test case where header doesn't have ID columns."""
+        out_stream = StringIO()
+        err_stream = StringIO()
+        errors = run(input_file=no_ids_stream,
+                     identifiers=identifiers_map,
+                     output_file=out_stream,
+                     error_file=err_stream)
+        assert errors
+
+    def test_data_with_matching_ids(self, data_stream: StringIO, identifiers_map: dict[Any, Any]):
+        """Test case where everything should match."""
+        out_stream = StringIO()
+        err_stream = StringIO()
+        errors = run(input_file=data_stream,
+                     identifiers=identifiers_map,
+                     output_file=out_stream,
+                     error_file=err_stream)
+        assert not errors
+        assert empty(err_stream)
+        assert not empty(out_stream)
+        out_stream.seek(0)
+        reader = csv.DictReader(out_stream, dialect='unix')
+        assert reader.fieldnames
+        assert 'naccid' in reader.fieldnames
+        row = next(reader)
+        assert row['naccid'] == 'NACC000001'
+        row = next(reader)
+        assert row['naccid'] == 'NACC000002'
+
+    def test_data_with_mismatched_ids(self, data_stream: StringIO,
+                                      mismatched_identifiers_map: dict[Any, Any]):
+        """Test case where there is no matching identifier."""
+        out_stream = StringIO()
+        err_stream = StringIO()
+        errors = run(input_file=data_stream,
+                     identifiers=mismatched_identifiers_map,
+                     output_file=out_stream,
+                     error_file=err_stream)
+        assert errors
+
+
+    # TODO: code assumes one adcid test that catch more than one
