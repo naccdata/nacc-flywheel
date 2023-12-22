b08e13bd7cbf83fa08df3c53c13ddfe75b7c2436
Ben Keller <bjkeller@vt.edu>
2023-12-22 21:24:19 +0000

    Initial code using visitor for transforming CSV to JSON

--- gear/csv_to_json_transformer/src/python/csv_app/main.py
+++ gear/csv_to_json_transformer/src/python/csv_app/main.py
@@ -1,19 +1,40 @@
 """Defines ADD DETAIL computation."""
 
 import logging
-from typing import Any, List
+from typing import Any, Dict, List, TextIO
 
 from flywheel_adaptor.flywheel_proxy import FlywheelProxy
+from inputs.csv_reader import CSVVisitor, read_csv
+from outputs.errors import ErrorWriter
 
 log = logging.getLogger(__name__)
 
-def run(*,
-        proxy: FlywheelProxy,
-        file):
+
+class JSONWriter(CSVVisitor):
+    """Visitor to write the row as JSON."""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def add_header(self, header: List[str]) -> bool:
+        # TODO: check expected fields in header
+        return False
+
+    def visit(self, row: Dict[str, Any], line_num: int) -> bool:
+        # TODO: do any transformations on row
+        # TODO: construct file name
+        # TODO: write file (needs context?)
+
+        return False
+
+
+def run(*, proxy: FlywheelProxy, csv_file: TextIO,
+        error_writer: ErrorWriter) -> bool:
     """Runs ADD DETAIL process.
-    
+
     Args:
       proxy: the proxy for the Flywheel instance
       file: flywheel file path
     """
-    pass
+
+    return read_csv(input_file=csv_file, error_writer=error_writer)
--- gear/csv_to_json_transformer/src/python/csv_app/run.py
+++ gear/csv_to_json_transformer/src/python/csv_app/run.py
@@ -1,20 +1,21 @@
-"""ADD DETAIL HERE"""
+"""Entrypoint script for the csv-to-json transformer app."""
 
 import logging
 import sys
-from flywheel_adaptor.flywheel_proxy import FlywheelProxy
+from pathlib import Path
 
-from flywheel_gear_toolkit import GearToolkitContext
 from csv_app.main import run
-
+from flywheel_adaptor.flywheel_proxy import FlywheelProxy
+from flywheel_gear_toolkit import GearToolkitContext
+from outputs.errors import ErrorWriter
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger(__name__)
 
+
 def main():
-    """Gear main method to transform CSV where row is participant data to
-    set of JSON files, one per participant.
-    """
+    """Gear main method to transform CSV where row is participant data to set
+    of JSON files, one per participant."""
 
     with GearToolkitContext() as gear_context:
         gear_context.init_logging()
@@ -26,11 +27,16 @@ def main():
         dry_run = gear_context.config.get("dry_run", False)
         flywheel_proxy = FlywheelProxy(client=client, dry_run=dry_run)
 
-        csv_file = gear_context.get_input_path('csv_file')
-
-
-    run(proxy=flywheel_proxy,
-        file=csv_file)
+        file_input = gear_context.get_input('input_file')
+        file_id = file_input['object']['file_id']
+        filename = gear_context.get_input_filename('csv_file')
+        input_path = Path(gear_context.get_input_path('csv_file'))
+        with open(input_path, mode='r', encoding='utf-8') as csv_file:
+            with gear_context.open_output(f'{filename}-error.csv') as err_file:
+                run(proxy=flywheel_proxy,
+                    csv_file=csv_file,
+                    error_writer=ErrorWriter(stream=err_file,
+                                             container_id=file_id))
 
     if __name__ == "__main__":
         main()
