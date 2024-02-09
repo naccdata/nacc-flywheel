0d5c0bc91f360068107e7cd7e7975fe1a16a99fa
Ben Keller <bjkeller@vt.edu>
2024-02-09 21:30:23 +0000

    Format code

--- common/test/python/identifiers/test_identifiers.py
+++ common/test/python/identifiers/test_identifiers.py
@@ -120,6 +120,7 @@ class TestIdentifierRepository:
 
         repo = IdentifierRepository(session)
         assert repo.list(adc_id=0) == expected
-        assert repo.list(adc_id=1) == [Identifier(nacc_id=2, nacc_adc=5397, adc_id=1,
-                       patient_id="168721")]
+        assert repo.list(adc_id=1) == [
+            Identifier(nacc_id=2, nacc_adc=5397, adc_id=1, patient_id="168721")
+        ]
         assert not repo.list(adc_id=99)
