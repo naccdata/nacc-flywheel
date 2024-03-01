5f8a05ad5e9f231101068c2c205b759f439e4271
Ben Keller <bjkeller@vt.edu>
2024-03-01 22:24:47 +0000

    Refactor center lookup

--- common/src/python/centers/nacc_group.py
+++ common/src/python/centers/nacc_group.py
@@ -6,6 +6,25 @@ from centers.center_group import CenterGroup
 from flywheel.models.group import Group
 from flywheel_adaptor.flywheel_proxy import (FlywheelProxy, GroupAdaptor,
                                              ProjectAdaptor)
+from pydantic import BaseModel, ValidationError
+
+
+class CenterInfo(BaseModel):
+    """Represents information about a center in nacc/metadata project.
+
+    Attributes:
+        adcid (int): The ADC ID of the center.
+        name (str): The name of the center.
+        group (str): The group ID of the center.
+    """
+    adcid: int
+    name: str
+    group: str
+
+
+class CenterMapInfo(BaseModel):
+    """Represents the center map in nacc/metadata project."""
+    centers: Dict[int, CenterInfo]
 
 
 class NACCGroup(GroupAdaptor):
@@ -63,14 +82,12 @@ class NACCGroup(GroupAdaptor):
         """
         center_map = self.get_center_map()
         metadata = self.get_metadata()
-        center_map[adcid] = {
-            'acdid': adcid,
-            'name': group_label,
-            'group': group_id
-        }
+        center_map[adcid] = CenterInfo(adcid=adcid,
+                                       name=group_label,
+                                       group=group_id)
         metadata.update_info({'centers': center_map})
 
-    def get_center_map(self) -> Dict[int, Dict[str, str | int]]:
+    def get_center_map(self) -> Dict[int, CenterInfo]:
         """Returns the adcid-group map.
 
         Returns:
@@ -79,4 +96,22 @@ class NACCGroup(GroupAdaptor):
         project = self.get_metadata()
         info = project.get_info()
 
-        return info.get('centers', {})
+        if not info:
+            return {}
+        
+        try:
+            center_map = CenterMapInfo.model_validate(info.get('centers', {}))
+        except ValidationError:
+            center_map = CenterMapInfo(centers={})
+
+        return center_map.centers
+
+    def get_center(self, adcid: int) -> CenterGroup:
+        center_map = self.get_center_map()
+        group_info = center_map.get(adcid, {})
+
+        group_id = group_info.group
+        assert group_id, "No center found for ADCID"
+        group = self.__fw.find_group(group_id=str(group_id))
+
+        return CenterGroup.create_from_group(group=group, proxy=self.__fw)
