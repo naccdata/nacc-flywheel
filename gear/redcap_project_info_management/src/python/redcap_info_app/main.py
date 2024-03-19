2195692ee688891720af72ad2a7fbd59b2f5ea14
Ben Keller <bjkeller@vt.edu>
2024-03-19 16:24:29 +0000

    Add code to add redcap projects to center info

--- gear/redcap_project_info_management/src/python/redcap_info_app/main.py
+++ gear/redcap_project_info_management/src/python/redcap_info_app/main.py
@@ -1,19 +1,34 @@
 """Defines REDCap Project Info Management."""
 
 import logging
-from typing import Any, List
+from typing import List
 
-from flywheel_adaptor.flywheel_proxy import FlywheelProxy
+from centers.center_group import REDCapProjectInput
+from centers.nacc_group import NACCGroup
 
 log = logging.getLogger(__name__)
 
-def run(*,
-        proxy: FlywheelProxy,
-        object_list: List[List[Any]],
-        new_only: bool = False):
-    """Runs ADD DETAIL process.
-    
+
+def run(*, project_list: List[REDCapProjectInput], admin_group: NACCGroup):
+    """Adds REDCap project information from the list to the center info object.
+
     Args:
-      proxy: the proxy for the Flywheel instance
+      project_list: the list of REDCap project information
+      admin_group: the NACC group object
     """
-    pass
+
+    center_map = admin_group.get_center_map()
+    id_map = {info.group: info for info in center_map.centers.values()}
+
+    for project_input in project_list:
+        center_info = id_map.get(project_input.center_id)
+        if not center_info:
+            log.error("Center %s not found", project_input.center_id)
+            continue
+
+        center_group = admin_group.get_center(center_info.adcid)
+        if not center_group:
+            log.error("Center with ADCID %s not found", center_info.adcid)
+            continue
+
+        center_group.add_redcap_project(project_input)
