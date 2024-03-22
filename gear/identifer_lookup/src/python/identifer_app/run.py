e7139a8257c68e6f72fa432009f608c1d96034ae
Ben Keller <bjkeller@vt.edu>
2024-03-22 17:41:32 +0000

    Refactor adcid lookup to use admin group

--- gear/identifer_lookup/src/python/identifer_app/run.py
+++ gear/identifer_lookup/src/python/identifer_app/run.py
@@ -3,10 +3,8 @@
 import logging
 import sys
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Dict
 
-from centers.center_group import CenterError, CenterGroup
-from flywheel_adaptor.flywheel_proxy import FlywheelProxy
 from flywheel_gear_toolkit import GearToolkitContext
 from gear_execution.gear_execution import (GearBotExecutionVisitor,
                                            GearExecutionEngine,
@@ -49,25 +47,6 @@ def get_identifiers(rds_parameters: RDSParameters,
     return identifiers
 
 
-def get_adcid(proxy: FlywheelProxy, file_id: str) -> Optional[int]:
-    """Get the adcid of the center group that owns the file.
-
-    Args:
-      proxy: the flwheel proxy object
-      file_id: the ID for the file
-    Returns:
-      the ADCID for the center
-    """
-    file = proxy.get_file(file_id)
-    group_id = file.parents.group
-    groups = proxy.find_groups(group_id)
-    try:
-        center = CenterGroup.create_from_group(group=groups[0], proxy=proxy)
-    except CenterError:
-        return None
-    return center.adcid
-
-
 class IdentifierLookupVisitor(GearBotExecutionVisitor):
     """The gear execution visitor for the identifier lookup app."""
 
@@ -106,21 +85,23 @@ class IdentifierLookupVisitor(GearBotExecutionVisitor):
         except ParameterError as error:
             raise GearExecutionError(f'Parameter error: {error}') from error
 
-    def run(self, gear: GearExecutionEngine):
+    def run(self, engine: GearExecutionEngine):
         """Runs the identifier lookup app.
 
         Args:
-            gear: the gear execution engine
+            engine: the gear execution engine
         """
         assert self.client, 'Flywheel client required'
         assert self.file_input, 'Input file required'
         assert self.rds_parameters, 'RDS parameters required'
-        assert gear.context, 'Gear context required'
+        assert engine.context, 'Gear context required'
 
         proxy = self.get_proxy()
+        admin_group = self.get_admin_group()
 
         file_id = self.file_input['object']['file_id']
-        adcid = get_adcid(proxy=proxy, file_id=file_id)
+        group_id = proxy.get_file_group(file_id)
+        adcid = admin_group.get_adcid(group_id)
         if not adcid:
             raise GearExecutionError('Unable to determine center ID for file')
 
@@ -132,15 +113,15 @@ class IdentifierLookupVisitor(GearBotExecutionVisitor):
         filename = f"{self.file_input['location']['name']}-identifier"
         input_path = Path(self.file_input['location']['path'])
         with open(input_path, mode='r', encoding='utf-8') as csv_file:
-            with gear.context.open_output(f'{filename}.csv',
-                                          mode='w',
-                                          encoding='utf-8') as out_file:
+            with engine.context.open_output(f'{filename}.csv',
+                                            mode='w',
+                                            encoding='utf-8') as out_file:
                 error_writer = ListErrorWriter(container_id=file_id)
                 errors = run(input_file=csv_file,
                              identifiers=identifiers,
                              output_file=out_file,
                              error_writer=error_writer)
-                gear.context.metadata.add_qc_result(
+                engine.context.metadata.add_qc_result(
                     self.file_input,
                     name="validation",
                     state="FAIL" if errors else "PASS",
