8a362c68561f213fa9108222fed3dec4892ab736
Chandima H <hewanadun@users.noreply.github.com>
2024-04-18 13:38:10 -0400

    redcap-fw-transfer-gear updates (#62)
    * Update retrieving REDCap project info
    
    * Add redcap report validation
    
    * Update version
    
    * Update error message

--- gear/redcap_fw_transfer/src/python/redcap_fw_transfer_app/main.py
+++ gear/redcap_fw_transfer/src/python/redcap_fw_transfer_app/main.py
@@ -1,13 +1,13 @@
 """Defines REDCap to Flywheel Transfer."""
+
 import json
 import logging
-import sys
 from datetime import datetime
 from typing import Any, Dict, List, TextIO
 
 import pandas as pd
-from flywheel_adaptor.flywheel_proxy import ProjectAdaptor
 from flywheel_gear_toolkit import GearToolkitContext
+from gear_execution.gear_execution import GearExecutionError
 from redcap.redcap_connection import (REDCapConnectionError,
                                       REDCapReportConnection)
 
@@ -18,18 +18,27 @@ def upload_to_flywheel(visits: List[Dict[str, Any]], output_file: TextIO):
     """Convert the visits details to CSV format and upload to Flywheel.
 
     Args:
-        visits (List[Dict[str, str]]): List of new/updated visits
-        output_file (TextIO): output file created in Flywheel ingest project
+        visits: List of new/updated visits
+        output_file: output file created in the Flywheel ingest project
+
+    Raises:
+        GearExecutionError if CSV upload fails
     """
 
     input_df = pd.DataFrame(visits)
-    # drop REDCap specific columns
-    output_df = input_df.drop([
+    redcap_labels = [
         'redcap_event_name', 'redcap_repeat_instrument',
         'redcap_repeat_instance', 'upld_ready___1'
-    ],
-                              axis=1)
-    output_df.to_csv(path_or_buf=output_file, index=False, doublequote=False)
+    ]
+    # drop REDCap longitudinal project columns if exists
+    output_df = input_df.drop(labels=redcap_labels, axis=1, errors='ignore')
+    try:
+        output_df.to_csv(path_or_buf=output_file,
+                         index=False,
+                         doublequote=False)
+    except Exception as error:
+        raise GearExecutionError(
+            f'Problem occurred while writing CSV: {error}') from error
 
 
 def reset_upload_checkbox(redcap_con: REDCapReportConnection,
@@ -37,16 +46,23 @@ def reset_upload_checkbox(redcap_con: REDCapReportConnection,
     """Reset the upload checkboxes in REDCap records.
 
     Args:
-        redcap_con (REDCapReportConnection): API connection to REDCap project
-        visits (List[Dict[str, str]]): List of new/updated visits
-        timestamp (datetime): Upload timestamp
+        redcap_con: API connection to REDCap project
+        visits: List of new/updated visits
+        timestamp: Upload timestamp
+
+    Raises:
+        GearExecutionError if failed to update REDCap records
     """
+
     updates = []
+
     for visit in visits:
         update = {}
-        update['ptid'] = visit['ptid']
-        update['redcap_event_name'] = visit['redcap_event_name']
-        update['redcap_repeat_instance'] = visit['redcap_repeat_instance']
+        update[redcap_con.primary_key_field] = visit[
+            redcap_con.primary_key_field]
+        if redcap_con.has_repeating_instruments_or_events():
+            update['redcap_event_name'] = visit['redcap_event_name']
+            update['redcap_repeat_instance'] = visit['redcap_repeat_instance']
         update['upld_ready___1'] = '0'
         update['upld_time'] = timestamp.strftime('%Y-%m-%d %H:%M:%S')
         updates.append(update)
@@ -55,12 +71,37 @@ def reset_upload_checkbox(redcap_con: REDCapReportConnection,
         num_updated = redcap_con.import_records(json.dumps(updates))
         log.info('Number of updated records: %s', num_updated)
     except REDCapConnectionError as error:
-        log.error(error.message)
-        sys.exit(1)
+        raise GearExecutionError(error.message) from error
 
 
-def run(*, gear_context: GearToolkitContext, fw_prj_adaptor: ProjectAdaptor,
-        redcap_con: REDCapReportConnection, redcap_pid: str):
+def validate_redcap_report(redcap_con: REDCapReportConnection,
+                           record: Dict[str, str]):
+    """Check whether the required fields included in the report.
+
+    Args:
+        redcap_con: API connection to REDCap project
+        record: sample record from the report
+
+    Raises:
+        GearExecutionError if required fields are missing in the report
+    """
+
+    if redcap_con.primary_key_field not in record:
+        raise GearExecutionError(
+            f'Primary key {redcap_con.primary_key_field} not included '
+            f'in project {redcap_con.pid} report {redcap_con.report_id}')
+
+    if redcap_con.has_repeating_instruments_or_events():
+        if ('redcap_event_name' not in record
+                or 'redcap_repeat_instance' not in record):
+            raise GearExecutionError(
+                f'REDCap repeating instance fields not included '
+                f'in project {redcap_con.pid} report {redcap_con.report_id}')
+
+
+def run(*, gear_context: GearToolkitContext,
+        redcap_con: REDCapReportConnection, redcap_pid: str, module: str,
+        fw_group: str, fw_project: str):
     """Download new/updated records from REDCap and upload to Flywheel as a CSV
     file.
 
@@ -68,24 +109,34 @@ def run(*, gear_context: GearToolkitContext, fw_prj_adaptor: ProjectAdaptor,
         fw_prj_adaptor: Flywheel project to transfer data
         redcap_con: API connection to REDCap project
         redcap_pid: REDCap project id
+        module: Forms module
+        fw_group: Flywheel group id
+        fw_project: Flywheel project label
+
+    Raises:
+        GearExecutionError if any problem occurs during the transfer
     """
 
     try:
         records_list = redcap_con.get_report_records()
     except REDCapConnectionError as error:
-        log.error(error.message)
-        sys.exit(1)
+        raise GearExecutionError(error.message) from error
 
     if len(records_list) == 0:
         log.info(
-            'No new/updated visits found in REDCap project '
-            '%s for Flywheel project %s/%s', redcap_pid, fw_prj_adaptor.group,
-            fw_prj_adaptor.label)
-        sys.exit(0)
+            'No new/updated visits found in REDCap project pid=%s module=%s '
+            'for Flywheel project %s/%s', redcap_pid, module, fw_group,
+            fw_project)
+        return
+
+    validate_redcap_report(redcap_con, records_list[0])
 
     timestamp = datetime.now()
-    file_name = 'udsv4_' + timestamp.strftime('%Y-%m-%d-%H%M%S') + '.csv'
+    file_name = 'redcap_ingest-' + timestamp.strftime(
+        '%Y%m%d_%H%M%S') + '-' + module + '.csv'
+
     with gear_context.open_output(file_name, mode='w',
                                   encoding='utf-8') as output_file:
         upload_to_flywheel(records_list, output_file)
+
     reset_upload_checkbox(redcap_con, records_list, timestamp)
--- gear/redcap_fw_transfer/src/python/redcap_fw_transfer_app/run.py
+++ gear/redcap_fw_transfer/src/python/redcap_fw_transfer_app/run.py
@@ -2,10 +2,17 @@
 
 import logging
 import sys
+from typing import Any, Dict, Optional, Tuple
 
-from flywheel import Client
-from flywheel_adaptor.flywheel_proxy import FlywheelProxy, ProjectAdaptor
+from centers.center_group import (CenterError, CenterGroup,
+                                  FormIngestProjectMetadata, REDCapFormProject)
+from flywheel.rest import ApiException
+from flywheel_adaptor.flywheel_proxy import FlywheelProxy, GroupAdaptor
 from flywheel_gear_toolkit import GearToolkitContext
+from gear_execution.gear_execution import (ClientWrapper, GearBotClient,
+                                           GearEngine,
+                                           GearExecutionEnvironment,
+                                           GearExecutionError)
 from inputs.context_parser import ConfigParseError, get_config
 from inputs.parameter_store import ParameterError, ParameterStore
 from redcap.redcap_connection import REDCapReportConnection
@@ -15,107 +22,216 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def get_destination_project_id(destination: dict, fw_client: Client) -> str:
-    """Find the flywheel project id of the destination project.
+def get_destination_group_and_project(dest_container: Any) -> Tuple[str, str]:
+    """Find the flywheel group id and project id for the destination project.
 
     Args:
-        destination (dict): gear_context.destination
-        fw_client (Client): Flywheel SDK client
+        dest_container: Flywheel container in which the gear is triggered
 
     Returns:
-        str: project id
+        Tuple[str, str]: group id, project id
+
+    Raises:
+        GearExecutionError if any error occurred while retrieving parent info
     """
 
-    if not destination or 'type' not in destination or 'id' not in destination:
-        log.error('Gear destination not set, specify the destination project')
-        sys.exit(1)
+    if not dest_container:
+        raise GearExecutionError('Gear destination not set')
 
-    dest_type = destination.get('type')
-    dest_id = destination.get('id')
-    if dest_type == 'project':
-        project_id = dest_id
-    elif dest_type in ('session', 'acquisition'):
-        getter = getattr(fw_client, f'get_{dest_type}')
-        dest_container = getter(dest_id)
-        project_id = dest_container.parents.get('project')
+    if dest_container.container_type == 'project':
+        project_id = dest_container.id
+        group_id = dest_container.group
+    elif dest_container.container_type in ('session', 'acquisition'):
+        project_id = dest_container.parents.project
+        group_id = dest_container.parents.group
     else:
-        log.error('Invalid gear destination %s', destination)
-        sys.exit(1)
+        raise GearExecutionError(
+            f'Invalid gear destination type {dest_container.container_type}')
 
-    return str(project_id)
+    return group_id, project_id
 
 
-# pylint: disable=(too-many-locals)
-def main():
-    """Main method for REDCap to Flywheel Transfer."""
+def get_redcap_projects_metadata(
+        *, fw_proxy: FlywheelProxy, group_adaptor: GroupAdaptor,
+        project_label: str) -> Dict[str, REDCapFormProject]:
+    """Retrieve the info on source REDCap projects to transfer the data from.
+    REDCap->FW mapping info is included in each center's metadata project.
 
-    with GearToolkitContext() as gear_context:
-        gear_context.init_logging()
-        gear_context.log_config()
+    Args:
+        fw_proxy: Flywheel proxy
+        group_adaptor: Flywhee adaptor for the center group
+        project_label: Flywheel ingest project label to upload data
 
-        default_client = gear_context.client
-        if not default_client:
-            log.error('Flywheel client required to confirm gearbot access')
-            sys.exit(1)
+    Returns:
+        Dict[str, REDCapFormProject]: REDCap project info by module
 
-        path_prefix = gear_context.config.get("apikey_path_prefix",
-                                              "/prod/flywheel/gearbot")
-        log.info('Running gearbot with API key from %s/apikey', path_prefix)
+    Raises:
+        GearExecutionError if any error occurred while parsing metadata
+    """
+
+    redcap_projects = {}
+
+    try:
+        center_group = CenterGroup.create_from_group_adaptor(
+            proxy=fw_proxy, adaptor=group_adaptor)
+    except CenterError as error:
+        raise GearExecutionError(
+            f'Error in creating CenterGroup: {error}') from error
+
+    center_metadata = center_group.get_project_info()
+    matches = 0
+    for study, study_metadata in center_metadata.studies.items():
+        # there should be only one study with matching project label
+        # iterate until a match found
+        project_metadata: Optional[
+            FormIngestProjectMetadata] = study_metadata.get_ingest(
+                project_label)  # type: ignore
+        if not project_metadata or not project_metadata.redcap_projects:
+            continue
+
+        matches += 1
+        log.info(
+            'REDCap projects metadata found for center: %s, '
+            'study: %s, project: %s', group_adaptor.id, study, project_label)
+        redcap_projects = project_metadata.redcap_projects
+
+    if matches > 1:
+        raise GearExecutionError(
+            'More than one match found for project '
+            f'{project_label} in center {group_adaptor.id} metadata')
+
+    return redcap_projects
+
+
+class REDCapFlywheelTransferVisitor(GearExecutionEnvironment):
+    """The gear execution visitor for the redcap_fw_transfer app."""
+
+    def __init__(self, client: ClientWrapper, parameter_store: ParameterStore,
+                 param_path: str):
+        """
+        Args:
+            client: Flywheel SDK client wrapper
+            parameter_store: AWS parameter store connection
+            param_path: AWS parameter path for REDCap credentials
+        """
+        self.__client = client
+        self.__param_store = parameter_store
+        self.__param_path = param_path
+
+    @classmethod
+    def create(
+        cls, context: GearToolkitContext,
+        parameter_store: Optional[ParameterStore]
+    ) -> 'REDCapFlywheelTransferVisitor':
+        """Creates a redcap_fw_transfer execution visitor.
+
+        Args:
+          context: the gear context
+          parameter_store: AWS parameter store connection
+
+        Raises:
+          GearExecutionError if any error occurred while parsing gear configs
+        """
+        assert parameter_store, "Parameter store expected"
+
         try:
-            parameter_store = ParameterStore.create_from_environment()
-
-            api_key = parameter_store.get_api_key(path_prefix=path_prefix)
-
-            param_path = str(
-                get_config(gear_context=gear_context, key='parameter_path'))
-        except ParameterError as error:
-            log.error('Parameter error: %s', error)
-            sys.exit(1)
+            param_path = get_config(gear_context=context, key='parameter_path')
         except ConfigParseError as error:
-            log.error('Incomplete configuration: %s', error.message)
-            sys.exit(1)
+            raise GearExecutionError(
+                f'Incomplete configuration: {error.message}') from error
 
-        host = gear_context.client.api_client.configuration.host  # type: ignore
-        if api_key.split(':')[0] not in host:
-            log.error('Gearbot API key does not match host')
-            sys.exit(1)
-
-        fw_client = Client(api_key)
-
-        project_id = get_destination_project_id(gear_context.destination,
-                                                fw_client)
-
-        dry_run = gear_context.config.get("dry_run", False)
-        fw_proxy = FlywheelProxy(fw_client, dry_run=dry_run)
-        fw_project = fw_client.get_project(project_id)
-        prj_adaptor = ProjectAdaptor(project=fw_project, proxy=fw_proxy)
-        redcap_prj_id = prj_adaptor.get_custom_project_info(
-            'redcap_project_id')
-        if not redcap_prj_id:
-            log.error('redcap_project_id not defined for project: %s/%s',
-                      prj_adaptor.group, prj_adaptor.label)
-            sys.exit(1)
+        client_wrapper = GearBotClient.create(context=context,
+                                              parameter_store=parameter_store)
 
         if not param_path.endswith('/'):
             param_path += '/'
-        param_path += 'pid_' + redcap_prj_id
-        log.info(param_path)
+
+        return REDCapFlywheelTransferVisitor(client=client_wrapper,
+                                             parameter_store=parameter_store,
+                                             param_path=param_path)
+
+    def run(self, context: GearToolkitContext):
+        """Runs the redcap_fw_transfer app.
+
+        Args:
+            context: the gear execution context
+
+        Raises:
+            GearExecutionError if error occurrs while transferring data
+        """
+
+        assert context, 'Gear context required'
+
+        fw_proxy = self.__client.get_proxy()
 
         try:
-            report_parameters = parameter_store.get_redcap_report_connection(
-                param_path=param_path)
+            dest_container = context.get_destination_container()
+        except ApiException as error:
+            raise GearExecutionError(
+                f'Cannot find destination container: {error}') from error
 
-        except ParameterError as error:
-            log.error('Parameter error: %s', error)
-            sys.exit(1)
+        group_id, project_id = get_destination_group_and_project(
+            dest_container)
+        group = fw_proxy.find_group(group_id)
+        if not group:
+            raise GearExecutionError(f'Cannot find Flywheel group {group_id}')
+        project = fw_proxy.get_project_by_id(project_id)
+        if not project:
+            raise GearExecutionError(
+                f'Cannot find Flywheel project {project_id}')
 
-        redcap_connection = REDCapReportConnection.create_from(
-            report_parameters)
+        redcap_projects = get_redcap_projects_metadata(
+            fw_proxy=fw_proxy,
+            group_adaptor=group,
+            project_label=project.label)
 
-        run(gear_context=gear_context,
-            fw_prj_adaptor=prj_adaptor,
-            redcap_con=redcap_connection,
-            redcap_pid=redcap_prj_id)
+        if not redcap_projects:
+            raise GearExecutionError(
+                'REDCap project information not found for '
+                f'{group_id}/{project.label}')
+
+        failed_count = 0
+        for redcap_project in redcap_projects.values():
+            redcap_project_path = (self.__param_path + 'pid_' +
+                                   str(redcap_project.redcap_pid) + '/' +
+                                   redcap_project.label)
+            try:
+                redcap_params = self.__param_store.get_redcap_report_connection(
+                    param_path=redcap_project_path)
+                redcap_report_con = REDCapReportConnection.create_from(
+                    redcap_params)
+            except ParameterError as error:
+                log.error(
+                    'Error in retrieving REDCap report credentials '
+                    'for project %s module %s: %s', redcap_project.redcap_pid,
+                    redcap_project.label, error)
+                failed_count += 1
+                continue
+
+            try:
+                run(gear_context=context,
+                    redcap_con=redcap_report_con,
+                    redcap_pid=str(redcap_project.redcap_pid),
+                    module=redcap_project.label,
+                    fw_group=group_id,
+                    fw_project=project.label)
+            except GearExecutionError as error:
+                log.error(
+                    'Error in ingesting module %s from REDCap project %s: %s',
+                    redcap_project.label, redcap_project.redcap_pid, error)
+                failed_count += 1
+                continue
+
+        if failed_count > 0:
+            raise GearExecutionError(
+                'Failed to ingest data for {failed_count} module(s)')
+
+
+def main():
+    """Main method for REDCap to Flywheel Transfer."""
+
+    GearEngine.create_with_parameter_store().run(
+        gear_type=REDCapFlywheelTransferVisitor)
 
 
 if __name__ == "__main__":
