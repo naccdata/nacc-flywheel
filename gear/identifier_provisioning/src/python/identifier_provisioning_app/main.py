0da0f3026c97ce26446e94faefd069e2f538f6ab
Ben Keller <bjkeller@vt.edu>
2024-04-19 21:39:25 +0000

    Update provisioning app

--- gear/identifier_provisioning/src/python/identifier_provisioning_app/main.py
+++ gear/identifier_provisioning/src/python/identifier_provisioning_app/main.py
@@ -1,20 +1,61 @@
 """Defines Identifier Provisioning."""
 
 import logging
-from typing import Any, List
+from typing import Any, Dict, List, TextIO
 
 from flywheel_adaptor.flywheel_proxy import FlywheelProxy
+from inputs.csv_reader import CSVVisitor, read_csv
+from outputs.errors import ErrorWriter
 
 log = logging.getLogger(__name__)
 
 
-def run(*,
-        proxy: FlywheelProxy,
-        object_list: List[List[Any]],
-        new_only: bool = False):
-    """Runs ADD DETAIL process.
+class ProvisioningVisitor(CSVVisitor):
+    """A CSV Visitor class for processing participant enrollment and transfer
+    forms."""
+
+    def __init__(self, error_writer: ErrorWriter) -> None:
+        self.__error_writer = error_writer
+
+    def visit_header(self, header: List[str]) -> bool:
+        """Prepares visitor to work with CSV file with given header.
+        
+        Args:
+          header: the list of header names
+        Returns:
+          True if something. False otherwise
+        """
+        return False
+
+    def visit_row(self, row: Dict[str, Any], line_num: int) -> bool:
+        """Provisions a NACCID for the NACCID and PTID.
+        
+        If not a transfer, checks that the center already has a participant
+        with PTID.
+        And, checks whether demographics match any existing participant.
+        In both case is an error.
+        
+        If is a transfer, BLAH
+        
+        Args:
+          row: the dictionary for the CSV row (DictReader)
+          line_num: the line number of the row
+        Returns:
+          True if a NACCID is provisioned without error, False otherwise
+        """
+        # if not a transfer, and PTID already has a NACCID => error
+        # if demographics match existing participant => error
+        # otherwise, create new NACCID
+        return False
+
+
+def run(*, input_file: TextIO, error_writer: ErrorWriter):
+    """Runs identifier provisioning process.
 
     Args:
-      proxy: the proxy for the Flywheel instance
+      input_file: the data input stream
+      error_writer: the error output writer
     """
-    pass
+    return read_csv(input_file=input_file,
+                    error_writer=error_writer,
+                    visitor=ProvisioningVisitor(error_writer=error_writer))
--- gear/identifier_provisioning/src/python/identifier_provisioning_app/run.py
+++ gear/identifier_provisioning/src/python/identifier_provisioning_app/run.py
@@ -1,22 +1,26 @@
 """Entry script for Identifier Provisioning."""
 
 import logging
-import sys
+from pathlib import Path
 from typing import Optional
 
+from centers.nacc_group import NACCGroup
+from flywheel_adaptor.flywheel_proxy import FlywheelError
 from flywheel_gear_toolkit import GearToolkitContext
 from gear_execution.gear_execution import (ClientWrapper, GearBotClient,
-                                           GearExecutionEngine,
+                                           GearEngine,
+                                           GearExecutionEnvironment,
                                            GearExecutionError,
-                                           GearExecutionVisitor,
                                            InputFileWrapper)
+from identifier_provisioning_app.main import run
 from inputs.parameter_store import (ParameterError, ParameterStore,
                                     RDSParameters)
+from outputs.errors import ListErrorWriter
 
 log = logging.getLogger(__name__)
 
 
-class IdentifierProvisioningVisitor(GearExecutionVisitor):
+class IdentifierProvisioningVisitor(GearExecutionEnvironment):
     """Execution visitor for NACCID provisioning gear."""
 
     def __init__(self, client: ClientWrapper, admin_id: str,
@@ -55,29 +59,44 @@ class IdentifierProvisioningVisitor(GearExecutionVisitor):
                                              file_input=file_input,
                                              rds_parameters=rds_parameters)
 
-    def run(self, gear: 'GearExecutionEngine') -> None:
-        pass
+    def run(self, context: GearToolkitContext) -> None:
+        """Runs the identifier provisioning app.
+
+        Args:
+          context: the gear execution context
+        """
+        assert context, 'Gear context required'
+
+        proxy = self.__client.get_proxy()
+        try:
+            admin_group = NACCGroup.create(proxy=proxy,
+                                           group_id=self.__admin_id)
+        except FlywheelError as error:
+            raise GearExecutionError(str(error)) from error
+
+        file_id = self.__file_input.file_id
+        group_id = proxy.get_file_group(file_id)
+        adcid = admin_group.get_adcid(group_id)
+        if not adcid:
+            raise GearExecutionError('Unable to determine center ID for file')
+
+        input_path = Path(self.__file_input.filepath)
+        with open(input_path, mode='r', encoding='utf-8') as csv_file:
+            error_writer = ListErrorWriter(container_id=file_id,
+                                           fw_path=proxy.get_lookup_path(
+                                               proxy.get_file(file_id)))
+            errors = run(input_file=csv_file, error_writer=error_writer)
+            context.metadata.add_qc_result(self.__file_input.file_input,
+                                           name="validation",
+                                           state="FAIL" if errors else "PASS",
+                                           data=error_writer.errors())
 
 
 def main():
     """Main method for Identifier Provisioning."""
 
-    try:
-        parameter_store = ParameterStore.create_from_environment()
-    except ParameterError as error:
-        log.error('Unable to create Parameter Store: %s', error)
-        sys.exit(1)
-
-    engine = GearExecutionEngine(parameter_store=parameter_store)
-
-    try:
-        engine.run(visitor_type=IdentifierProvisioningVisitor)
-    except GearExecutionError as error:
-        log.error('Error: %s', error)
-        sys.exit(1)
-
-    if __name__ == "__main__":
-        main()
+    GearEngine.create_with_parameter_store().run(
+        gear_type=IdentifierProvisioningVisitor)
 
 
 if __name__ == "__main__":
