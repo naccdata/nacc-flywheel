f8a99214000f8a43bc6b426f539de2cc13079e21
Ben Keller <bjkeller@vt.edu>
2024-06-08 17:46:37 +0000

    Update run.py for change to using lambdas

--- gear/identifier_provisioning/src/python/identifier_provisioning_app/run.py
+++ gear/identifier_provisioning/src/python/identifier_provisioning_app/run.py
@@ -13,8 +13,10 @@ from gear_execution.gear_execution import (ClientWrapper, GearBotClient,
                                            GearExecutionError,
                                            InputFileWrapper)
 from identifier_provisioning_app.main import run
-from inputs.parameter_store import (ParameterError, ParameterStore,
-                                    RDSParameters)
+from identifiers.identifiers_lambda_repository import (
+    IdentifiersLambdaRepository, IdentifiersMode)
+from inputs.parameter_store import ParameterStore
+from lambdas.lambda_function import LambdaClient, create_lambda_client
 from outputs.errors import ListErrorWriter
 
 log = logging.getLogger(__name__)
@@ -23,13 +25,15 @@ log = logging.getLogger(__name__)
 class IdentifierProvisioningVisitor(GearExecutionEnvironment):
     """Execution visitor for NACCID provisioning gear."""
 
+    # pylint: disable=(too-many-arguments)
     def __init__(self, client: ClientWrapper, admin_id: str,
-                 file_input: InputFileWrapper,
-                 rds_parameters: RDSParameters) -> None:
+                 file_input: InputFileWrapper, form_name: str,
+                 identifiers_mode: IdentifiersMode) -> None:
         self.__client = client
         self.__admin_id = admin_id
         self.__file_input = file_input
-        self.__rds_parameters = rds_parameters
+        self.__form_name = form_name
+        self.__identifiers_mode: IdentifiersMode = identifiers_mode
 
     @classmethod
     def create(
@@ -42,22 +46,15 @@ class IdentifierProvisioningVisitor(GearExecutionEnvironment):
                                       parameter_store=parameter_store)
         file_input = InputFileWrapper.create(input_name='input_file',
                                              context=context)
-
-        rds_param_path = context.config.get('rds_parameter_path')
-        if not rds_param_path:
-            raise GearExecutionError('No value for rds_parameter_path')
-
-        try:
-            rds_parameters = parameter_store.get_rds_parameters(
-                param_path=rds_param_path)
-        except ParameterError as error:
-            raise GearExecutionError(f'Parameter error: {error}') from error
         admin_id = context.config.get("admin_group", "nacc")
+        mode = context.config.get("identifiers_mode", "dev")
+        form_name = context.config.get("form_name", "ptenrlv1")
 
         return IdentifierProvisioningVisitor(client=client,
                                              admin_id=admin_id,
                                              file_input=file_input,
-                                             rds_parameters=rds_parameters)
+                                             identifiers_mode=mode,
+                                             form_name=form_name)
 
     def run(self, context: GearToolkitContext) -> None:
         """Runs the identifier provisioning app.
@@ -65,7 +62,12 @@ class IdentifierProvisioningVisitor(GearExecutionEnvironment):
         Args:
           context: the gear execution context
         """
+
         assert context, 'Gear context required'
+
+        identifiers_repo = IdentifiersLambdaRepository(
+            client=LambdaClient(client=create_lambda_client()),
+            mode=self.__identifiers_mode)
 
         proxy = self.__client.get_proxy()
         try:
@@ -85,7 +87,10 @@ class IdentifierProvisioningVisitor(GearExecutionEnvironment):
             error_writer = ListErrorWriter(container_id=file_id,
                                            fw_path=proxy.get_lookup_path(
                                                proxy.get_file(file_id)))
-            errors = run(input_file=csv_file, error_writer=error_writer)
+            errors = run(input_file=csv_file,
+                         form_name=self.__form_name,
+                         error_writer=error_writer,
+                         repo=identifiers_repo)
             context.metadata.add_qc_result(self.__file_input.file_input,
                                            name="validation",
                                            state="FAIL" if errors else "PASS",
