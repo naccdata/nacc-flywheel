634f47ab7ded8c7ee1d7591097636bedb56dc183
Ben Keller <bjkeller@vt.edu>
2024-06-09 20:49:40 +0000

    Removed commented out code

--- common/src/python/enrollment/enrollment_transfer.py
+++ common/src/python/enrollment/enrollment_transfer.py
@@ -33,6 +33,8 @@ class Demographics(BaseModel):
         """Constructs a Demographics object from row of enrollment/transfer
         form.
 
+        Assumes form is PTENRLv1.
+
         Args:
           row: the dictionary for the row of form.
         Returns:
@@ -52,72 +54,6 @@ class Demographics(BaseModel):
                                 other_term=row['enrlgenothx'],
                                 dont_know=row['enrlgendkn'],
                                 no_answer=row['enrlgennoans']))
-
-
-# class EnrollmentTransferRow(BaseModel):
-#     adcid: int = Field(ge=0)
-#     ptid: str = Field(max_length=10)
-#     frmdate_enrl: str  # date
-#     initials_enrl: str
-#     enrltype: Literal[1, 2]
-#     enrlbirthmo: int = Field(ge=1, le=12)
-#     enrlbirthyr: int = Field(ge=1950, le=2030)
-#     enrleduc: int | Literal[99] = Field(ge=0, le=36)
-#     enrlgenman: Optional[Literal[1]]
-#     enrlgenwoman: Optional[Literal[1]]
-#     enrlgentrman: Optional[Literal[1]]
-#     enrlgentrwoman: Optional[Literal[1]]
-#     enrlgennonbi: Optional[Literal[1]]
-#     enrlgentwospir: Optional[Literal[1]]
-#     enrlgenoth: Optional[Literal[1]]
-#     enrlgenothx: Optional[Literal[1]]
-#     enrlgendkn: Optional[Literal[1]]
-#     enrlgennoans: Optional[Literal[1]]
-#     guidavail: Literal[0, 1]
-#     guid: str
-#     prevenrl: Literal[1, 2]
-#     oldadcid: int = Field(ge=0)
-#     oldptid: str = Field(max_length=10)
-#     naccidknwn: Literal[1, 2]
-#     naccid: str = Field(min_length=10, pattern=r"^NACC\d{6}$")
-#     ptidconf: str = Field(max_length=10)
-#     module: str
-
-#     @property
-#     def new_enrollment(self) -> bool:
-#         return self.enrltype == 1
-
-#     @property
-#     def previously_enrolled(self) -> bool:
-#         return self.prevenrl == 1
-
-#     @property
-#     def naccid_known(self) -> bool:
-#         return self.naccidknwn == 1
-
-#     @property
-#     def current_identifiers(self) -> CenterIdentifiers:
-#         return CenterIdentifiers(adcid=self.adcid, ptid=self.ptid)
-
-#     @property
-#     def previous_identifiers(self) -> CenterIdentifiers:
-#         return CenterIdentifiers(adcid=self.oldadcid, ptid=self.oldptid)
-
-#     def demographics(self) -> Demographics:
-#         return Demographics(years_education=self.enrleduc,
-#                             birth_date=datetime(int(self.enrlbirthmo),
-#                                                 int(self.enrlbirthyr), 1),
-#                             gender_identity=GenderIdentity(
-#                                 man=self.enrlgenman,
-#                                 woman=self.enrlgenwoman,
-#                                 transgender_man=self.enrlgentrman,
-#                                 transgender_woman=self.enrlgentrwoman,
-#                                 nonbinary=self.enrlgennonbi,
-#                                 two_spirit=self.enrlgentwospir,
-#                                 other=self.enrlgenoth,
-#                                 other_term=self.enrlgenothx,
-#                                 dont_know=self.enrlgendkn,
-#                                 no_answer=self.enrlgennoans))
 
 
 class TransferRecord(BaseModel):
