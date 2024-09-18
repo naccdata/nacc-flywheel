1e3fde10d80eb74e21f581b4d54824632922c7fd
Ben Keller <bjkeller@vt.edu>
2024-09-18 20:36:32 +0000

    Rename test file

---
+++ common/test/python/users/test_user_entry.py
@@ -0,0 +1,89 @@
+"""Tests for user directory."""
+
+import yaml
+from users.authorizations import Authorizations
+from users.nacc_directory import ActiveUserEntry, PersonName, UserEntry
+
+
+# pylint: disable=(no-self-use,too-few-public-methods)
+class TestUserEntry:
+    """Tests for UserEntry."""
+
+    def test_inactive(self):
+        """Tests for creating inactive objects."""
+        entry = UserEntry(name=PersonName(first_name='ooly',
+                                          last_name='puppy'),
+                          email='ools@that.org',
+                          auth_email='ools@that.org',
+                          active=False)
+        entry2 = UserEntry.create_from_record({
+            "contact_company_name":
+            "the center",
+            "adresearchctr":
+            "0",
+            "firstname":
+            "ooly",
+            "lastname":
+            "puppy",
+            "email":
+            "ools@that.org",
+            "fw_email":
+            "ools@that.org",
+            "flywheel_access_activities":
+            "a,c,d,e",
+            "nacc_data_platform_access_information_complete":
+            "2",
+            "archive_contact":
+            "1"
+        })
+        assert entry == entry2
+        entry_yaml = yaml.safe_dump(entry.as_dict())
+        entry_object = yaml.safe_load(entry_yaml)
+        print(entry_object)
+        entry3 = UserEntry.create(entry_object)
+        assert entry == entry3
+
+    def test_active(self):
+        """Tests around creating objects."""
+        entry = ActiveUserEntry(org_name='the center',
+                                adcid=0,
+                                name=PersonName(first_name='chip',
+                                                last_name='puppy'),
+                                email='chip@theorg.org',
+                                authorizations=Authorizations(
+                                    submit=['form', 'enrollment'],
+                                    audit_data=True,
+                                    approve_data=True,
+                                    view_reports=True),
+                                active=True,
+                                auth_email='chip_auth@theorg.org')
+
+        assert entry.authorizations.audit_data
+
+        entry2 = UserEntry.create_from_record({
+            "contact_company_name":
+            "the center",
+            "adresearchctr":
+            "0",
+            "firstname":
+            "chip",
+            "lastname":
+            "puppy",
+            "email":
+            "chip@theorg.org",
+            "fw_email":
+            "chip_auth@theorg.org",
+            "flywheel_access_activities":
+            "a,c,d,e",
+            "nacc_data_platform_access_information_complete":
+            "2",
+            "archive_contact":
+            "0"
+        })
+        assert entry == entry2
+
+        entry_yaml = yaml.safe_dump(entry.as_dict())
+        entry_object = yaml.safe_load(entry_yaml)
+        print(entry_object)
+        entry3 = UserEntry.create(entry_object)
+        assert entry == entry3
