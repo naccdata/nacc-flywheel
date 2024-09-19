8123e4bcfa9f2fbc3d0dd89b64b5405534646426
Ben Keller <bjkeller@vt.edu>
2024-09-19 21:55:11 +0000

    Change pull-directory to use user entry list root model

--- gear/pull_directory/src/python/directory_app/main.py
+++ gear/pull_directory/src/python/directory_app/main.py
@@ -3,7 +3,7 @@ import logging
 from typing import Any, Dict, List
 
 import yaml
-from users.nacc_directory import UserEntry
+from users.nacc_directory import UserEntry, UserEntryList
 
 log = logging.getLogger(__name__)
 
@@ -16,20 +16,20 @@ def run(*, user_report: List[Dict[str, Any]]) -> str:
       user_report: user report records
     """
 
-    user_map = {}
+    user_list = UserEntryList([])
+    user_emails = set()
     for user_record in user_report:
         entry = UserEntry.create_from_record(user_record)
         if not entry:
             continue
 
-        if entry.email in user_map:
-            log.warning('Email %s occurs in more than one contact', entry.email)
-        
+        if entry.email in user_emails:
+            log.warning('Email %s occurs in more than one contact',
+                        entry.email)
 
-        user_map[entry.email] = entry
+        user_list.append(entry)
+        user_emails.add(entry.email)
 
-    entries = [entry.as_dict() for entry in user_map.values()]
-
-    return yaml.safe_dump(data=entries,
+    return yaml.safe_dump(data=user_list.model_dump(serialize_as_any=True),
                           allow_unicode=True,
                           default_flow_style=False)
