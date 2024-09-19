398a00eae46f5a7f7c76ec84999a227f4116fdf4
Ben Keller <bjkeller@vt.edu>
2024-09-19 14:23:01 +0000

    simplify pull-directory

--- gear/pull_directory/src/python/directory_app/main.py
+++ gear/pull_directory/src/python/directory_app/main.py
@@ -3,7 +3,7 @@ import logging
 from typing import Any, Dict, List
 
 import yaml
-from users.nacc_directory import UserDirectory, UserDirectoryEntry
+from users.nacc_directory import UserEntry
 
 log = logging.getLogger(__name__)
 
@@ -16,19 +16,19 @@ def run(*, user_report: List[Dict[str, Any]]) -> str:
       user_report: user report records
     """
 
-    directory = UserDirectory()
+    user_map = {}
     for user_record in user_report:
-        entry = UserDirectoryEntry.create_from_record(user_record)
+        entry = UserEntry.create_from_record(user_record)
         if not entry:
             continue
 
-        directory.add(entry)
+        if entry.email in user_map:
+            log.warning('Email %s occurs in more than one contact', entry.email)
+        
 
-    entries = [entry.as_dict() for entry in directory.get_entries()]
+        user_map[entry.email] = entry
 
-    # this call logs any conflicts
-    directory.get_conflicts()
-    # TODO: want to write to metadata for userfile instead
+    entries = [entry.as_dict() for entry in user_map.values()]
 
     return yaml.safe_dump(data=entries,
                           allow_unicode=True,
