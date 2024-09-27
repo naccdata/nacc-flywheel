0cba504bcfd22fa914029937ad66dc1b6f237ef6
Ben Keller <bjkeller@vt.edu>
2024-09-27 22:21:38 +0000

    Format code

--- common/test/python/notifications/test_email_client.py
+++ common/test/python/notifications/test_email_client.py
@@ -1,15 +1,12 @@
 import os
+
 import boto3
 import pytest
 from moto import mock_aws
 from moto.core.models import DEFAULT_ACCOUNT_ID
 from moto.ses.models import ses_backends
-
- 
-
 from notifications.email import DestinationModel, EmailClient, TemplateDataModel
 
-        # ses_backend = ses_backends[DEFAULT_ACCOUNT_ID][region]
 
 @pytest.fixture(scope="function")
 def aws_credentials():
@@ -18,11 +15,13 @@ def aws_credentials():
     os.environ['AWS_ACCESS_KEY_ID'] = "testing"
     os.environ['AWS_DEFAULT_REGION'] = "us-east-1"
 
+
 @pytest.fixture(scope="function")
 def ses(aws_credentials):
     """Fixture for mocking SES service."""
     with mock_aws():
-        yield boto3.client('ses', region_name="us-east-1") # types: ignore
+        yield boto3.client('ses', region_name="us-east-1")  # types: ignore
+
 
 @mock_aws
 class TestEmailClient:
@@ -30,7 +29,17 @@ class TestEmailClient:
     def test_client(self, ses):
         backend = ses_backends[DEFAULT_ACCOUNT_ID]["us-east-1"]
         backend.addresses.append('dummy@dummy.org')
-        backend.add_template({"template_name": "dummy", "subject_part": "blah", "html_part": "blah", "text_part": "blah"})
+        backend.add_template({
+            "template_name": "dummy",
+            "subject_part": "blah",
+            "html_part": "blah",
+            "text_part": "blah"
+        })
         email_client = EmailClient(client=ses, source='dummy@dummy.org')
-        response = email_client.send(configuration_set_name="blah",destination=DestinationModel(to_addresses=["dummy@dummy.dummy"]), template='dummy', template_data=TemplateDataModel(firstname="dummy", email_address="dummy@dummy.com"))
+        response = email_client.send(
+            configuration_set_name="blah",
+            destination=DestinationModel(to_addresses=["dummy@dummy.dummy"]),
+            template='dummy',
+            template_data=TemplateDataModel(firstname="dummy",
+                                            email_address="dummy@dummy.com"))
         assert response
