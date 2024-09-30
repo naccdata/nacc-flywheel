17efad9d4b89ab5b73bb8f8aa4326dadfdd6a694
Dan Peterson <djpeters@uw.edu>
2024-09-30 00:24:54 +0000

    create empty gear from cookiecutter template

---
+++ gear/redcap_refresh_dl_urls/src/python/redcap_refresh_dl_urls_app/run.py
@@ -0,0 +1,57 @@
+"""Entry script for Refresh REDCap Download URLs."""
+
+import logging
+
+from typing import Optional
+
+from centers.nacc_group import NACCGroup
+from flywheel_gear_toolkit import GearToolkitContext
+from gear_execution.gear_execution import (ClientWrapper, ContextClient,
+                                           GearEngine,
+                                           GearExecutionEnvironment)
+from redcap_refresh_dl_urls_app.main import run
+from inputs.parameter_store import ParameterStore
+from inputs.yaml import YAMLReadError, get_object_lists
+
+log = logging.getLogger(__name__)
+
+class REDCapRefreshDownloadURLs(GearExecutionEnvironment):
+    """Visitor for the templating gear."""
+
+    def __init__(self, admin_id: str, client: ClientWrapper, new_only: bool):
+        super().__init__(client=client, admin_id=admin_id)
+
+    @classmethod
+    def create(
+        cls,
+        context: GearToolkitContext,
+        parameter_store: Optional[ParameterStore] = None
+    ) -> 'REDCapRefreshDownloadURLs':
+        """Creates a gear execution object.
+
+        Args:
+            context: The gear context.
+            parameter_store: The parameter store
+        Returns:
+          the execution environment
+        Raises:
+          GearExecutionError if any expected inputs are missing
+        """
+        client = ContextClient.create(context=context)
+
+        return REDCapRefreshDownloadURLs(
+            admin_id=context.config.get("admin_group", "nacc"),
+            client=client,
+            new_only=context.config.get("new_only", False))
+
+    def run(self, context: GearToolkitContext) -> None:
+        run(proxy=self.proxy,
+            new_only=.self.__new_only)
+        
+def main():
+    """Main method for Refresh REDCap Download URLs."""
+
+    GearEngine().run(gear_type=REDCapRefreshDownloadURLs)
+
+if __name__ == "__main__":
+    main()
