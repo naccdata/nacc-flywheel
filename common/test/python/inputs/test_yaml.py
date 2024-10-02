76bbeb3898a61726ef6192ecf1026c301a81fb3f
Ben Keller <bjkeller@vt.edu>
2024-10-02 18:02:58 +0000

    Add tests to yaml

--- common/test/python/inputs/test_yaml.py
+++ common/test/python/inputs/test_yaml.py
@@ -22,8 +22,28 @@ class TestYAML:
         yaml_stream = StringIO()
         yaml_stream.write(yaml_object)
         object_list = get_object_lists_from_stream(yaml_stream)
-        assert object_list == []
+        assert object_list is not None
+        assert [doc for doc in object_list] == []
 
         yaml_stream.seek(0)
         yaml_object = load_from_stream(yaml_stream)
         assert yaml_object == []
+
+    def test_load_all(self):
+        yaml_stream = StringIO()
+        yaml_stream.write("---\n"
+                          "k1: v1\n"
+                          "k2: v2\n"
+                          "---\n"
+                          "k1: v1\n"
+                          "k2: v2\n")
+        yaml_stream.seek(0)
+        yaml_iterator = get_object_lists_from_stream(yaml_stream)
+        object_list = [object for object in yaml_iterator]
+        assert object_list == [{
+            'k1': 'v1',
+            'k2': 'v2'
+        }, {
+            'k1': 'v1',
+            'k2': 'v2'
+        }]
