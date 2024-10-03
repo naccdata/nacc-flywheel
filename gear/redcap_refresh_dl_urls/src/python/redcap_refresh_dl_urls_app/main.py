cc5a5a880fb09bb586055874011719371ca8f6e6
Dan Peterson <djpeters@uw.edu>
2024-10-03 22:04:34 +0000

    gear works locally

--- gear/redcap_refresh_dl_urls/src/python/redcap_refresh_dl_urls_app/main.py
+++ gear/redcap_refresh_dl_urls/src/python/redcap_refresh_dl_urls_app/main.py
@@ -1,18 +1,72 @@
 """Defines Refresh REDCap Download URLs."""
 
 import logging
-
+import json
+from redcap.redcap_connection import REDCapConnection
 from flywheel_adaptor.flywheel_proxy import FlywheelProxy
 
 log = logging.getLogger(__name__)
 
+def refresh_url(project, redcap_con, subject_label='',reader_email=''):
+
+    print(f'refreshing urls for subject: {subject_label} and reader: {reader_email}')
+    
+    subject = project.subjects.find_one(f'label={subject_label}')
+
+    session = subject.sessions()[0]
+
+    acquisition = session.acquisitions()[0]
+
+    for file in acquisition.files:
+        if file.name.endswith('.zip'):
+            dicom_file = file
+
+    file_download_url = acquisition.get_file_download_url(dicom_file.name)
+
+    print(f'file_download_url: {file_download_url}')
+
+    record = {
+        'record_id' : f'{reader_email}-{subject_label}',
+        'download_link' : file_download_url
+    }
+
+    record_str = json.dumps([record])
+
+    #print(record_str)
+
+    response = redcap_con.import_records(record_str)
+
+    return response
+
 def run(*,
-        proxy: FlywheelProxy):
+        proxy: FlywheelProxy, redcap_con):
     """Runs ADD DETAIL process.
     
     Args:
       proxy: the proxy for the Flywheel instance
     """
+
+
+    #subject_label = 'calibration004'
+    #reader_email = 'djpeters@uw.edu'
+
+    project_id='66df6928917dfee283a2769b'
+    project = proxy.get_project_by_id(project_id)
+
+    #print(redcap_con.export_field_names())
+
+    print(redcap_con.export_records())
+
+    existing_records=redcap_con.export_records()
+
+    for record in existing_records:
+
+        print(record)
+
+        response = refresh_url(project, redcap_con,
+                               subject_label=record['subject'],
+                               reader_email=record['reader_email'])
+        print(response)
 
     print('hello world')
 
--- gear/redcap_refresh_dl_urls/src/python/redcap_refresh_dl_urls_app/run.py
+++ gear/redcap_refresh_dl_urls/src/python/redcap_refresh_dl_urls_app/run.py
@@ -1,8 +1,11 @@
 """Entry script for Refresh REDCap Download URLs."""
 
 import logging
+import os
+import boto3
 
 from typing import Optional
+from dotenv import load_dotenv
 
 from flywheel_gear_toolkit import GearToolkitContext
 from gear_execution.gear_execution import (ClientWrapper, ContextClient,
@@ -10,6 +13,7 @@ from gear_execution.gear_execution import (ClientWrapper, ContextClient,
                                            GearExecutionEnvironment)
 from redcap_refresh_dl_urls_app.main import run
 from inputs.parameter_store import ParameterStore
+from redcap.redcap_connection import REDCapConnection
 
 log = logging.getLogger(__name__)
 
@@ -40,12 +44,31 @@ class REDCapRefreshDownloadURLs(GearExecutionEnvironment):
         return REDCapRefreshDownloadURLs(client=client)
 
     def run(self, context: GearToolkitContext) -> None:
-        run(proxy=self.proxy)
+
+        ssm = boto3.client('ssm')
+        parameter = ssm.get_parameter(Name='/redcap/aws/pid_83/token', WithDecryption=True)
+        token = parameter['Parameter']['Value']
+        url = 'https://redcap.naccdata.org/api/'
+        #print(f'token: {token}')
+
+        redcap_con = REDCapConnection(token=token, url=url)
+        
+        run(proxy=self.proxy, redcap_con=redcap_con)
         
 def main():
     """Main method for Refresh REDCap Download URLs."""
 
-    GearEngine().run(gear_type=REDCapRefreshDownloadURLs)
+    context = GearToolkitContext()
+
+    env_file_path = context.get_input_path('dotenv')
+        
+    if env_file_path is not None:
+        print('loading environment variables from file')
+        load_dotenv(env_file_path, override=True)
+
+    #print(os.environ)
+
+    GearEngine.create_with_parameter_store().run(gear_type=REDCapRefreshDownloadURLs)
 
 if __name__ == "__main__":
     main()
