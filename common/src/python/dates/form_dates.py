26b678236fe4553e10fa7254b3a84f260b02db9e
Ben Keller <bjkeller@vt.edu>
2024-10-06 20:04:38 +0000

    Add form date parsing

---
+++ common/src/python/dates/form_dates.py
@@ -0,0 +1,32 @@
+"""Utilities to handle dates."""
+
+from datetime import datetime
+from typing import List
+
+DATE_FORMATS = ['%m/%d/%Y', '%Y-%m-%d']
+
+
+def parse_date(*, date_string: str, formats: List[str]) -> datetime:
+    """Parses the date string against the list of formats.
+
+    Args:
+      date_string: a date as a string
+    Returns:
+      the datetime object for the string
+    Raises:
+      DateFormatException if the string doesn't match either format
+    """
+
+    for format in formats:
+        try:
+            return datetime.strptime(date_string, format)
+        except ValueError:
+            pass
+
+    raise DateFormatException(formats=formats)
+
+
+class DateFormatException(Exception):
+
+    def __init__(self, formats: List[str]) -> None:
+        self.formats = formats
