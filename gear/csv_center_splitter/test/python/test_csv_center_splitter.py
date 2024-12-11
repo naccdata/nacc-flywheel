fdd7691c56dd6033c9a318bd866566f256383f7e
Emily Cheng <echeng06@uw.edu>
2024-12-10 18:04:39 -0800

    Ignore some format changes

--- gear/csv_center_splitter/test/python/test_csv_center_splitter.py
+++ gear/csv_center_splitter/test/python/test_csv_center_splitter.py
@@ -22,27 +22,13 @@ def visitor(error_writer):
 @pytest.fixture(scope='module')
 def merged_csv_data():
     """Creates dummy CSV data with merged cells."""
-    return [{
-        'adcid': '1',
-        'data': 'dummy_value',
-        'extra': ''
-    }, {
-        'adcid': '',
-        'data': 'dummy_value_2',
-        'extra': ''
-    }, {
-        'adcid': '',
-        'data': '',
-        'extra': ''
-    }, {
-        'adcid': '2',
-        'data': 'hello',
-        'extra': ''
-    }, {
-        'adcid': '',
-        'data': 'world',
-        'extra': ''
-    }]
+    return [
+        {'adcid': '1', 'data': 'dummy_value', 'extra': ''},
+        {'adcid': '', 'data': 'dummy_value_2', 'extra': ''},
+        {'adcid': '', 'data': '', 'extra': ''},
+        {'adcid': '2', 'data': 'hello', 'extra': ''},
+        {'adcid': '', 'data': 'world', 'extra': ''}
+    ]
 
 
 class TestCSVVisitorCenterSplitter:
