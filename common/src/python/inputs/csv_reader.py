1c15b73c2b7813b27f421ea1d0b64b3a8b874397
Emily Cheng <echeng06@uw.edu>
2024-12-13 12:48:55 -0800

    Refactor read_csv to remove Sniffer

--- common/src/python/inputs/csv_reader.py
+++ common/src/python/inputs/csv_reader.py
@@ -2,7 +2,7 @@
 
 import abc
 from abc import ABC, abstractmethod
-from csv import DictReader, Error, Sniffer
+from csv import DictReader, Error
 from typing import Any, Dict, List, Optional, TextIO
 
 from outputs.errors import (
@@ -39,45 +39,43 @@ class CSVVisitor(ABC):
         return True
 
 
-def read_csv(input_file: TextIO, error_writer: ErrorWriter,
-             visitor: CSVVisitor) -> bool:
+def read_csv(input_file: TextIO,
+             error_writer: ErrorWriter,
+             visitor: CSVVisitor,
+             delimiter: str = ',') -> bool:
     """Reads CSV file and applies the visitor to each row.
 
     Args:
       input_file: the input stream for the CSV file
       error_writer: the ErrorWriter for the input file
       visitor: the visitor
+      delimiter: Expected delimiter for the CSV
     Returns:
       True if the input file was processed without error, False otherwise
     """
-    sniffer = Sniffer()
     csv_sample = input_file.read(1024)
     if not csv_sample:
         error_writer.write(empty_file_error())
         return False
 
-    try:
-        has_header = sniffer.has_header(csv_sample)
-    except Error as error:
-        error_writer.write(malformed_file_error(str(error)))
-        return False
+    input_file.seek(0)
 
-    if not has_header:
+    reader = DictReader(input_file, delimiter=delimiter)
+    if not reader.fieldnames:
         error_writer.write(missing_header_error())
         return False
-
-    input_file.seek(0)
-    detected_dialect = sniffer.sniff(csv_sample, delimiters=',')
-    reader = DictReader(input_file, dialect=detected_dialect)
-    assert reader.fieldnames, "File has header, reader should have fieldnames"
 
     success = visitor.visit_header(list(reader.fieldnames))
     if not success:
         return False
 
-    for record in reader:
-        row_success = visitor.visit_row(record, line_num=reader.line_num)
-        success = row_success and success
+    try:
+        for record in reader:
+            row_success = visitor.visit_row(record, line_num=reader.line_num)
+            success = row_success and success
+    except Error as error:
+        error_writer.write(malformed_file_error(str(error)))
+        return False
 
     return success
 
