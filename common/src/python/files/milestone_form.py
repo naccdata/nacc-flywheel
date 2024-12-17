af89c7f86da1cca2938b88bec64216d1d6cbb2e1
Ben Keller <bjkeller@vt.edu>
2024-12-17 16:35:01 +0000

    Add init to milestone form

--- common/src/python/files/milestone_form.py
+++ common/src/python/files/milestone_form.py
@@ -3,11 +3,16 @@ from datetime import datetime
 from typing import Optional
 
 from dates.dates import datetime_from_form_date
+
 from files.form import Form
+from flywheel.models.file_entry import FileEntry
 
 
 class MilestoneForm(Form):
     """Milestone form class used for attribute curation."""
+    
+    def __init__(self, file_object: FileEntry) -> None:
+        super().__init__(file_object)
 
     def get_session_date(self) -> Optional[datetime]:
         """Get date of Milestones session.
