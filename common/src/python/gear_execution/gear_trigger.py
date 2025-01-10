bce5d8f466b2b25c5614d389902f5e1391be3be4
Emily Cheng <echeng06@uw.edu>
2025-01-10 21:05:48 +0000

    Allow extra configs

--- common/src/python/gear_execution/gear_trigger.py
+++ common/src/python/gear_execution/gear_trigger.py
@@ -15,7 +15,8 @@ log = logging.getLogger(__name__)
 
 class GearConfigs(BaseModel):
     """Class to represent base gear configs."""
-    model_config = ConfigDict(populate_by_name=True)
+    model_config = ConfigDict(populate_by_name=True,
+                              extra='allow')
 
     apikey_path_prefix: str
     dry_run: bool
